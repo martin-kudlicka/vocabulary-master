05c6e0ba73976a0c24a766fdc8ad2a3bb4bcfb87
Isshou <Isshou@0d4cc249-e473-e245-99fb-1f26886583cc>
2011-07-03 12:25:48 +0000

    * pass pressed keys to parent class

--- vocabularymanagerdialog/vocabularyview.cpp
+++ vocabularymanagerdialog/vocabularyview.cpp
@@ -9,6 +9,8 @@ void VocabularyView::keyPressEvent(QKeyEvent *event)
 	if (event->matches(QKeySequence::Paste)) {
 		model()->setData(currentIndex(), QApplication::clipboard()->text());
 	} // if
+
+	QTableView::keyPressEvent(event);
 } // keyPressEvent
 
 VocabularyView::VocabularyView(QWidget *pParent /* NULL */) : QTableView(pParent)
