2ba21712218b844446c10f47e5a0598ff92a3275
Isshou <37011117+martin-kudlicka@users.noreply.github.com>
2011-07-05 00:04:45 +0000

    + load plug-ins on startup

---
+++ source/plugins.cpp
@@ -0,0 +1,26 @@
+#include "plugins.h"
+
+#include <QtCore/QDir>
+#include <QtCore/QCoreApplication>
+#include <QtCore/QPluginLoader>
+
+#ifndef _DEBUG
+const QString DIR_PLUGINS = "plugins";
+#endif
+
+const void Plugins::Load()
+{
+	QDir qdPlugins(QCoreApplication::applicationDirPath());
+#ifndef _DEBUG
+	qdPlugins.cd(DIR_PLUGINS);
+#endif
+	foreach (QString qsFileName, qdPlugins.entryList(QDir::Files)) {
+		QPluginLoader qplLoader(qdPlugins.absoluteFilePath(qsFileName));
+		if (qplLoader.instance()) {
+			TTSInterface *tiPlugin = qobject_cast<TTSInterface *>(qplLoader.instance());
+			if (tiPlugin) {
+				_qhTTSPlugins.insert(tiPlugin->GetPluginId(), tiPlugin);
+			} // if
+		} // if
+	} // foreach
+} // Load
---
+++ source/plugins.h
@@ -0,0 +1,16 @@
+#ifndef PLUGINS_H
+#define PLUGINS_H
+
+#include <QtCore/QHash>
+#include "../plugins/common/tts-interface.h"
+
+class Plugins
+{
+	public:
+		const void Load();
+
+	private:
+		QHash<TTSInterface::eTTSPlugin, TTSInterface *> _qhTTSPlugins;
+}; // Plugins
+
+#endif // PLUGINS_H
