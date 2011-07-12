b041974a4bfe09e65a0558567ba7f62efc922324
isshou.cz <isshou.cz@0d4cc249-e473-e245-99fb-1f26886583cc>
2011-07-12 20:10:41 +0000

    + TTS-SAPI plug-in (no speech yet)

---
+++ plugins/tts-sapi/tts-sapi.h
@@ -0,0 +1,23 @@
+#ifndef TTSSAPI_H
+#define TTSSAPI_H
+
+#include "../common/tts-interface.h"
+#include <QtCore/QHash>
+
+class TTSSAPI : public QObject, private TTSInterface
+{
+    Q_OBJECT
+    Q_INTERFACES(TTSInterface)
+
+    private:
+        QHash<QString, QString> _qhVoices;
+
+        const eTTSPlugin GetPluginId() const;
+        const QString GetPluginName() const;
+		const QStringList GetVoicesIds() const;
+		const void Initialize();
+		const void Say(const QString &pVoice, const QString &pText);
+		const void Uninitialize();
+}; // TTSSAPI
+
+#endif // TTSSAPI_H
