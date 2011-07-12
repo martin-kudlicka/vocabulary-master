b05224fb7a900f7c7744d28fe9bb37b30ca10a20
isshou.cz <isshou.cz@0d4cc249-e473-e245-99fb-1f26886583cc>
2011-07-12 17:35:50 +0000

    * better answer handling

--- source/mainwindow.h
+++ source/mainwindow.h
@@ -28,21 +28,24 @@ class MainWindow : public QMainWindow
 			bool bDirectionSwitched;
 		}; // sAnswer
 
+        typedef QHash<int, sAnswer> tAnswer;
+
 		int _iCurrentWord;
-		int _iTimerLearing;
+        int _iTimerAnswer;
+		int _iTimerQuestion;
 		Plugins _pPlugins;
-		sAnswer _saAnswer;
 		Settings _sSettings;
+        tAnswer _taHash;
 		Ui::qmwUiMain _umwMainWindow;
         Vocabulary _vVocabulary;
 
         const void ApplySettings(const bool &pStartup);
         const void EnableControls();
-		const QString GetLangColumn(const bool &pAnswer) const;
+		const QString GetLangColumn(const bool &pDirectionSwitched, const bool &pAnswer) const;
 		const bool GetLearningDirection() const;
-        const QString GetLearningText(const bool &pAnswer) const;
-        const QString GetNoteColumn(const bool &pAnswer) const;
-		const void Say(const bool &pAnswer) const;
+        const QString GetLearningText(const bool &pDirectionSwitched, const bool &pAnswer) const;
+        const QString GetNoteColumn(const bool &pDirectionSwitched, const bool &pAnswer) const;
+		const void Say(const bool &pDirectionSwitched, const bool &pAnswer) const;
         const void SetLayout();
 		void timerEvent(QTimerEvent *event);
 
@@ -54,7 +57,6 @@ class MainWindow : public QMainWindow
 		const void on_qaSettings_triggered(bool checked = false);
 		const void on_qaStart_triggered(bool checked = false);
 		const void on_qaStop_triggered(bool checked = false);
-		const void OnShowTranslation();
 }; // MainWindow
 
 #endif // MAINWINDOW_H
