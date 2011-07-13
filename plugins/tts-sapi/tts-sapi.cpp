3e67f142fc45d9edfe8b7c66af8fa1db5e1a6655
isshou.cz <isshou.cz@0d4cc249-e473-e245-99fb-1f26886583cc>
2011-07-13 08:37:48 +0000

    + speaking by Microsoft SAPI

--- plugins/tts-sapi/tts-sapi.cpp
+++ plugins/tts-sapi/tts-sapi.cpp
@@ -26,12 +26,12 @@ const QStringList TTSSAPI::GetVoicesIds() const
 	ccpVoices->GetCount(&ulCount);
 
 	for(ULONG ulI = 0; ulI < ulCount; ulI++) {
-		CComPtr<ISpObjectToken> ccpVoice;
+		CComPtr<ISpObjectToken> ccpVoiceInfo;
 		WCHAR *wcId;
 
-		ccpVoices->Next(1, &ccpVoice, NULL);
-		ccpVoice->GetId(&wcId);
-		//SpGetDescription(ccpVoice, &wcDescription);
+		ccpVoices->Next(1, &ccpVoiceInfo, NULL);
+		ccpVoiceInfo->GetId(&wcId);
+		//SpGetDescription(ccpVoiceInfo, &wcDescription);
 
 		qslVoiceIds.append(QString::fromWCharArray(wcId));
 	} // for
@@ -46,7 +46,27 @@ const void TTSSAPI::Initialize()
 
 const void TTSSAPI::Say(const QString &pVoice, const QString &pText)
 {
+    CComPtr<IEnumSpObjectTokens> ccpVoices;
+    ULONG ulCount;
 
+    SpEnumTokens(SPCAT_VOICES, NULL, NULL, &ccpVoices);
+    ccpVoices->GetCount(&ulCount);
+
+    for(ULONG ulI = 0; ulI < ulCount; ulI++) {
+        CComPtr<ISpObjectToken> ccpVoiceInfo;
+        WCHAR *wcId;
+
+        ccpVoices->Next(1, &ccpVoiceInfo, NULL);
+        ccpVoiceInfo->GetId(&wcId);
+
+        if (pVoice == QString::fromWCharArray(wcId)) {
+            CComPtr<ISpVoice> ccpVoice;
+            ccpVoice.CoCreateInstance(CLSID_SpVoice);
+            ccpVoice->SetVoice(ccpVoiceInfo);
+            ccpVoice->Speak(reinterpret_cast<LPCWSTR>(pText.unicode()), SPF_IS_NOT_XML, NULL);
+            //ccpVoiceInfo->Speak();
+        } // if
+    } // for
 } // Say
 
 const void TTSSAPI::Uninitialize()
