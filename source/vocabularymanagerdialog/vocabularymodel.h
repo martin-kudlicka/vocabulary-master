83a7a5ddb95bbd45c97884d187c8f4c9d090f01b
isshou.cz <37011117+martin-kudlicka@users.noreply.github.com>
2011-07-14 17:39:31 +0000

    + language description in vocabulary manager

--- source/vocabularymanagerdialog/vocabularymodel.h
+++ source/vocabularymanagerdialog/vocabularymodel.h
@@ -30,6 +30,7 @@ class VocabularyModel : public QAbstractTableModel
         int columnCount(const QModelIndex &parent = QModelIndex()) const;
         QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
 		Qt::ItemFlags flags(const QModelIndex &index) const;
+        QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
 		bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole);
 }; // VocabularyModel
 
