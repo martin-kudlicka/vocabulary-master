1a7a0dc08d38f379cae292d4c948522fb9b54eaa
isshou.cz <isshou.cz@0d4cc249-e473-e245-99fb-1f26886583cc>
2011-07-19 18:33:51 +0000

    + Czech translation

---
+++ source/lang/cs_CZ.ts
@@ -0,0 +1,401 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs_CZ" sourcelanguage="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="39"/>
+        <source>Exit</source>
+        <translation>Ukončit</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="156"/>
+        <source> FREE</source>
+        <translation>ZDARMA</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="213"/>
+        <source>Create new vocabulary</source>
+        <translation>Vytvořit nový slovník</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="237"/>
+        <source>Open vocabulary</source>
+        <translation>Otevřít slovník</translation>
+    </message>
+</context>
+<context>
+    <name>VocabularyManagerDialog</name>
+    <message>
+        <location filename="../vocabularymanagerdialog.cpp" line="55"/>
+        <source>Add category</source>
+        <translation>Přidat kategorii</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog.cpp" line="55"/>
+        <source>New category name</source>
+        <translation>Název nové kategorie</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog.cpp" line="137"/>
+        <source>Import words</source>
+        <translation>Importovat slovíčka</translation>
+    </message>
+</context>
+<context>
+    <name>VocabularyModel</name>
+    <message>
+        <location filename="../vocabularymanagerdialog/vocabularymodel.cpp" line="3"/>
+        <source>Note (%1)</source>
+        <translation>Pozn. (%1)</translation>
+    </message>
+</context>
+<context>
+    <name>VocabularySettingsDialog</name>
+    <message>
+        <location filename="../vocabularymanagerdialog/vocabularysettingsdialog.cpp" line="41"/>
+        <source>Unknown</source>
+        <translation>Neznámý</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog/vocabularysettingsdialog.cpp" line="57"/>
+        <source>None</source>
+        <translation>Žádný</translation>
+    </message>
+</context>
+<context>
+    <name>WordsImportDialog</name>
+    <message>
+        <location filename="../vocabularymanagerdialog/wordsimportdialog.cpp" line="23"/>
+        <source>Words import</source>
+        <translation>Import slovíček</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog/wordsimportdialog.cpp" line="23"/>
+        <source>Can&apos;t import data from selected file.</source>
+        <translation>Slovíčka nelze z vybraného souboru importovat.</translation>
+    </message>
+</context>
+<context>
+    <name>qdUiSettings</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>Settings</source>
+        <translation>Nastavení</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="24"/>
+        <source>General</source>
+        <translation>Obecné</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="36"/>
+        <source>Always on top</source>
+        <translation>Vždy nahoře</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="49"/>
+        <source>Remember window position</source>
+        <translation>Zapamatovat pozici okna</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="62"/>
+        <source>Horizontal layout</source>
+        <translation>Horizontální rozvržení</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="75"/>
+        <source>System tray icon</source>
+        <translation>Ikona v system tray</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="88"/>
+        <source>Minimize to tray</source>
+        <translation>Minimalizovat do tray</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="101"/>
+        <source>Show words in tray balloon</source>
+        <translation>Zobrazovat slovíčka v balónové nápovědě</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="114"/>
+        <source>Language:</source>
+        <translation>Jazyk:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="130"/>
+        <source>Learning</source>
+        <translation>Učení</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="142"/>
+        <location filename="../settingsdialog.ui" line="184"/>
+        <source>s</source>
+        <translation>s</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="158"/>
+        <source>Words frequency</source>
+        <translation>Frekvence slovíček</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="171"/>
+        <source>Play sound with new word</source>
+        <translation>Přehrát zvuk s novým slovíčkem</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="197"/>
+        <source>Wait for answer</source>
+        <translation>Počkat na odpověď</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="210"/>
+        <source>Switch learning direction</source>
+        <translation>Změnit směr učení</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="226"/>
+        <source>Start learning on startup</source>
+        <translation>Zahájit učení při spuštění aplikace</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="239"/>
+        <source>Flash with new word</source>
+        <translation>Vizuální upozornění na nové slovíčko</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="245"/>
+        <source>Appearance</source>
+        <translation>Vzhled</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="257"/>
+        <source>Word:</source>
+        <translation>Slovo:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="270"/>
+        <source>Note:</source>
+        <translation>Poznámka:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="283"/>
+        <location filename="../settingsdialog.ui" line="296"/>
+        <source>px</source>
+        <translation>px</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="309"/>
+        <source>Flash color:</source>
+        <translation>Barva upozornění:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="349"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="356"/>
+        <source>Cancel</source>
+        <translation>Zrušit</translation>
+    </message>
+</context>
+<context>
+    <name>qdVocabularyManager</name>
+    <message>
+        <location filename="../vocabularymanagerdialog.ui" line="14"/>
+        <source>Vocabulary Manager</source>
+        <translation>Správce slovíček</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog.ui" line="35"/>
+        <source>Find</source>
+        <translation>Najít</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog.ui" line="57"/>
+        <location filename="../vocabularymanagerdialog.ui" line="130"/>
+        <source>Word</source>
+        <translation>Slovíčka</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog.ui" line="73"/>
+        <source>Search</source>
+        <translation>Hledat</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog.ui" line="91"/>
+        <source>Category</source>
+        <translation>Kategorie</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog.ui" line="103"/>
+        <source>Add...</source>
+        <translation>Přidat...</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog.ui" line="116"/>
+        <location filename="../vocabularymanagerdialog.ui" line="155"/>
+        <source>Remove</source>
+        <translation>Odebrat</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog.ui" line="142"/>
+        <source>&amp;Add...</source>
+        <translation>&amp;Přidat...</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog.ui" line="168"/>
+        <source>Import...</source>
+        <translation>Odebrat...</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog.ui" line="176"/>
+        <source>Settings...</source>
+        <translation>Nastavení...</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog.ui" line="196"/>
+        <source>Close</source>
+        <translation>Zavřít</translation>
+    </message>
+</context>
+<context>
+    <name>qdVocabularySettingsDialog</name>
+    <message>
+        <location filename="../vocabularymanagerdialog/vocabularysettingsdialog.ui" line="14"/>
+        <source>Vocabulary Settings</source>
+        <translation>Nastavení slovníku</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog/vocabularysettingsdialog.ui" line="48"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog/vocabularysettingsdialog.ui" line="55"/>
+        <source>Cancel</source>
+        <translation>Zrušit</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog/vocabularysettingsdialog.ui" line="71"/>
+        <source>Speech</source>
+        <translation>Řeč</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog/vocabularysettingsdialog.ui" line="83"/>
+        <location filename="../vocabularymanagerdialog/vocabularysettingsdialog.ui" line="142"/>
+        <source>Left:</source>
+        <translation>Levý:</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog/vocabularysettingsdialog.ui" line="96"/>
+        <location filename="../vocabularymanagerdialog/vocabularysettingsdialog.ui" line="155"/>
+        <source>Right:</source>
+        <translation>Pravý:</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog/vocabularysettingsdialog.ui" line="130"/>
+        <source>Language</source>
+        <translation>Jazyk</translation>
+    </message>
+</context>
+<context>
+    <name>qdWordsImport</name>
+    <message>
+        <location filename="../vocabularymanagerdialog/wordsimportdialog.ui" line="14"/>
+        <source>Words Import</source>
+        <translation>Import slovíček</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog/wordsimportdialog.ui" line="48"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../vocabularymanagerdialog/wordsimportdialog.ui" line="55"/>
+        <source>Cancel</source>
+        <translation>Zrušit</translation>
+    </message>
+</context>
+<context>
+    <name>qmwUiMain</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>Vocabulary Master</source>
+        <translation>Vocabulary Master</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="30"/>
+        <location filename="../mainwindow.ui" line="59"/>
+        <source>TextLabel</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="87"/>
+        <source>&amp;File</source>
+        <translation>&amp;Soubor</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="96"/>
+        <source>&amp;Options</source>
+        <translation>&amp;Nastavení</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="102"/>
+        <source>&amp;Vocabulary</source>
+        <translation>S&amp;lovník</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="127"/>
+        <source>E&amp;xit</source>
+        <translation>&amp;Ukončit</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="132"/>
+        <source>&amp;Settings...</source>
+        <translation>&amp;Nastavení...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="137"/>
+        <source>&amp;New...</source>
+        <translation>&amp;Nový...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="142"/>
+        <source>&amp;Open...</source>
+        <translation>&amp;Otevřít...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="147"/>
+        <source>&amp;Manage...</source>
+        <translation>&amp;Spravovat...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="152"/>
+        <source>Start</source>
+        <translation>Spustit</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="157"/>
+        <source>Stop</source>
+        <translation>Zastavit</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="162"/>
+        <source>Next</source>
+        <translation>Další</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="170"/>
+        <source>Mute</source>
+        <translation>Ztlumit</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="175"/>
+        <source>Answer</source>
+        <translation>Odpověď</translation>
+    </message>
+</context>
+</TS>
