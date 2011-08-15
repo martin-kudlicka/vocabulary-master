2771bb012d5e5ca763eb00c8d216603b2ab684a1
vocabulary-master.cz <37011117+martin-kudlicka@users.noreply.github.com>
2011-08-15 18:07:00 +0000

    * codec list fixed

--- plugins/imp-plaintext/plaintextimportwidget/codecsmodel.cpp
+++ plugins/imp-plaintext/plaintextimportwidget/codecsmodel.cpp
@@ -2,6 +2,11 @@
 
 #include <QtCore/QTextCodec>
 
+CodecsModel::CodecsModel(QObject *pParent /* NULL */) : QAbstractItemModel(pParent)
+{
+    _qlCodecs = QTextCodec::availableCodecs();
+} // CodecsModel
+
 int CodecsModel::columnCount(const QModelIndex &parent /* QModelIndex() */) const
 {
     return ColumnCount;
@@ -11,15 +16,11 @@ QVariant CodecsModel::data(const QModelIndex &index, int role /* Qt::DisplayRole
 {
     switch (role) {
         case Qt::DisplayRole:
-            return QTextCodec::availableCodecs().at(index.row());
+            return _qlCodecs.at(index.row());
         default:
             return QVariant();
     } // switch
 } // data
-
-CodecsModel::CodecsModel(QObject *pParent /* NULL */) : QAbstractItemModel(pParent)
-{
-} // CodecsModel
 
 QVariant CodecsModel::headerData(int section, Qt::Orientation orientation, int role /* Qt::DisplayRole */) const
 {
@@ -44,7 +45,7 @@ QModelIndex CodecsModel::parent(const QModelIndex &index) const
 int CodecsModel::rowCount(const QModelIndex &parent /* QModelIndex() */) const
 {
     if (parent == QModelIndex()) {
-        return QTextCodec::availableCodecs().size();
+        return _qlCodecs.size();
     } else {
         return 0;
     } // if else
