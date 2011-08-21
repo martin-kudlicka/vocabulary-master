30c1ce8dc02a42740fabf8050a1b2c05231a47ab
vocabulary-master.cz <37011117+martin-kudlicka@users.noreply.github.com>
2011-08-21 12:48:29 +0000

    * categories model made common for import and export dialogs

--- source/vocabularymanagerdialog/wordsexportdialog.cpp
+++ source/vocabularymanagerdialog/wordsexportdialog.cpp
@@ -1,9 +1,11 @@
 #include "vocabularymanagerdialog/wordsexportdialog.h"
 
-WordsExportDialog::WordsExportDialog(const Vocabulary *pVocabulary, const Plugins::tExpPluginList &pExpPlugins, QWidget *pParent /* NULL */, Qt::WindowFlags pFlags /* 0 */) : QDialog(pParent, pFlags)
+WordsExportDialog::WordsExportDialog(const Vocabulary *pVocabulary, const Plugins::tExpPluginList &pExpPlugins, QWidget *pParent /* NULL */, Qt::WindowFlags pFlags /* 0 */) : QDialog(pParent, pFlags), _cmCategoriesModel(pVocabulary)
 {
 	_vVocabulary = pVocabulary;
 	_teplExpPlugins = pExpPlugins;
 
 	_qdweWordsExport.setupUi(this);
+
+	_qdweWordsExport.qtvCategories->setModel(&_cmCategoriesModel);
 } // WordsExportDialog
