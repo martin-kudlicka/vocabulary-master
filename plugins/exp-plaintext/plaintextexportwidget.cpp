fba28d58e6ac3c616b05951975b0fd75f075b794
vocabulary-master.cz <37011117+martin-kudlicka@users.noreply.github.com>
2011-08-21 15:17:10 +0000

    + plain text part of export widget

---
+++ plugins/exp-plaintext/plaintextexportwidget.cpp
@@ -0,0 +1,6 @@
+#include "plaintextexportwidget.h"
+
+PlaintextExportWidget::PlaintextExportWidget(QWidget *pParent /* NULL */, Qt::WindowFlags pFlags /* 0 */) : QWidget(pParent, pFlags)
+{
+    _qwpePlaintextExport.setupUi(this);
+} // PlaintextExportWidget
