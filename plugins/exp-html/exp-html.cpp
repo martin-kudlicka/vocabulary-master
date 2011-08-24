9deac793598a8da93078d8d9847c908c89266f1c
vocabulary-master.cz <vocabulary-master.cz@0d4cc249-e473-e245-99fb-1f26886583cc>
2011-08-24 18:10:41 +0000

    + HTML export plug-in

---
+++ plugins/exp-html/exp-html.cpp
@@ -0,0 +1,93 @@
+#include "exp-html.h"
+
+#include <QtGui/QFileDialog>
+#include <QtCore/QTextStream>
+
+const bool ExpHtml::BeginExport() const
+{
+    // get filename
+    QString qsFile = QFileDialog::getSaveFileName(_hewWidget, QString(), QString(), tr("html (*.html)"));
+    if (qsFile.isEmpty()) {
+        return false;
+    } // if
+
+    // open file
+    QFile qfFile(qsFile);
+    qfFile.open(QIODevice::WriteOnly | QIODevice::Text);
+
+    // text stream
+    QTextStream qtsTextStream(&qfFile);
+    qtsTextStream.setCodec(_hewWidget->GetCodec().toLocal8Bit());
+
+    // process export
+    _hewWidget->setUpdatesEnabled(false);
+    _hewWidget->Refresh();
+
+    // save result to file
+    qtsTextStream << _hewWidget->GetText();
+
+    return true;
+} // BeginExport
+
+const QString ExpHtml::GetPluginName() const
+{
+	return tr("html (html)");
+} // GetPluginName
+
+const void ExpHtml::on_hewWidget_ProgressExportSetMax(const int &pMax) const
+{
+    emit ProgressExportSetMax(pMax);
+} // on_hewWidget_ProgressExportSetMax
+
+const void ExpHtml::on_hewWidget_ProgressExportSetValue(const int &pValue) const
+{
+    emit ProgressExportSetValue(pValue);
+} // on_hewWidget_ProgressExportSetValue
+
+const void ExpHtml::on_hewWidget_VocabularyGetCategoryIds(ExpInterface::tCategoryIdList *pCategoryIds) const
+{
+    emit VocabularyGetCategoryIds(pCategoryIds);
+} // on_hewWidget_VocabularyGetCategoryIds
+
+const void ExpHtml::on_hewWidget_VocabularyGetCategoryName(const int &pCategoryId, QString *pName) const
+{
+    emit VocabularyGetCategoryName(pCategoryId, pName);
+} // on_hewWidget_VocabularyGetCategoryName
+
+const void ExpHtml::on_hewWidget_VocabularyGetMarks(QStringList *pMarks) const
+{
+    emit VocabularyGetMarks(pMarks);
+} // on_hewWidget_VocabularyGetMarks
+
+const void ExpHtml::on_hewWidget_VocabularyGetMarkText(const int &pRecordId, const QString &pMark, QString *pText) const
+{
+    emit VocabularyGetMarkText(pRecordId, pMark, pText);
+} // on_hewWidget_VocabularyGetMarkText
+
+const void ExpHtml::on_hewWidget_VocabularyGetRecordCount(const int &pCategoryId, int *pCount) const
+{
+    emit VocabularyGetRecordCount(pCategoryId, pCount);
+} // on_hewWidget_VocabularyGetRecordCount
+
+const void ExpHtml::on_hewWidget_VocabularyGetRecordIds(const int &pCategoryId, ExpInterface::tRecordIdList *pRecordIds) const
+{
+    emit VocabularyGetRecordIds(pCategoryId, pRecordIds);
+} // on_hewWidget_VocabularyGetRecordCount
+
+const void ExpHtml::SetupUI(QWidget *pParent)
+{
+	_hewWidget = new HtmlExportWidget(pParent);
+	QBoxLayout *pLayout = qobject_cast<QBoxLayout *>(pParent->layout());
+	pLayout->insertWidget(WIDGET_POSITION, _hewWidget);
+
+    connect(_hewWidget, SIGNAL(ProgressExportSetMax(const int &)), SLOT(on_hewWidget_ProgressExportSetMax(const int &)));
+    connect(_hewWidget, SIGNAL(ProgressExportSetValue(const int &)), SLOT(on_hewWidget_ProgressExportSetValue(const int &)));
+    connect(_hewWidget, SIGNAL(VocabularyGetCategoryIds(ExpInterface::tCategoryIdList *)), SLOT(on_hewWidget_VocabularyGetCategoryIds(ExpInterface::tCategoryIdList *)));
+    connect(_hewWidget, SIGNAL(VocabularyGetCategoryName(const int &, QString *)), SLOT(on_hewWidget_VocabularyGetCategoryName(const int &, QString *)));
+    connect(_hewWidget, SIGNAL(VocabularyGetMarks(QStringList *)), SLOT(on_hewWidget_VocabularyGetMarks(QStringList *)));
+    connect(_hewWidget, SIGNAL(VocabularyGetMarkText(const int &, const QString &, QString *)), SLOT(on_hewWidget_VocabularyGetMarkText(const int &, const QString &, QString *)));
+    connect(_hewWidget, SIGNAL(VocabularyGetRecordCount(const int &, int *)), SLOT(on_hewWidget_VocabularyGetRecordCount(const int &, int *)));
+    connect(_hewWidget, SIGNAL(VocabularyGetRecordIds(const int &, ExpInterface::tRecordIdList *)), SLOT(on_hewWidget_VocabularyGetRecordIds(const int &, ExpInterface::tRecordIdList *)));
+} // SetupUI
+
+Q_EXPORT_PLUGIN2(exp-html, ExpHtml)
---
+++ plugins/exp-html/htmlexportwidget.cpp
@@ -0,0 +1,249 @@
+#include "htmlexportwidget.h"
+
+#include <QtGui/QTextTable>
+#include <QtGui/QScrollBar>
+
+const void HtmlExportWidget::AddTableColumn()
+{
+    sTableColumn stcColumn;
+    stcColumn.qleHeader = new QLineEdit(_qwheHtmlExport.qwPageTable);
+    stcColumn.qleTemplate = new QLineEdit(_qwheHtmlExport.qwPageTable);
+    _qlTableColumns.append(stcColumn);
+
+    _qwheHtmlExport.qglTableColumns->addWidget(stcColumn.qleHeader, TableRowHeader, _qlTableColumns.size() + LABEL_COLUMN);
+    _qwheHtmlExport.qglTableColumns->addWidget(stcColumn.qleTemplate, TableRowTemplate, _qlTableColumns.size() + LABEL_COLUMN);
+} // AddTableColumn
+
+const QString HtmlExportWidget::GetCodec() const
+{
+    QModelIndex qmiIndex = _qwheHtmlExport.qtvCodecs->currentIndex();
+    return _cmCodecsModel.data(qmiIndex).toString();
+} // GetCodec
+
+const QString HtmlExportWidget::GetText() const
+{
+    if (_qwheHtmlExport.qrbStyleText->isChecked()) {
+        return _qwheHtmlExport.qteTextPreview->toHtml();
+    } else {
+        return _qwheHtmlExport.qteTablePreview->toHtml();
+    } // if else
+} // GetText
+
+const void HtmlExportWidget::InitTableColumns()
+{
+    for (int iColumn = 0; iColumn < _qwheHtmlExport.qsbTableColums->value(); iColumn++) {
+        AddTableColumn();
+    } // for
+} // InitTableColumns
+
+const void HtmlExportWidget::InsertTableText(const QTextTable *pTablePreview, const int &pRow, const int &pColumn, const QString &pText) const
+{
+    QTextTableCell qttcCell = pTablePreview->cellAt(pRow, pColumn);
+    QTextCursor qtcCursor = qttcCell.firstCursorPosition();
+    qtcCursor.insertText(pText);
+} // InsertTableText
+
+const void HtmlExportWidget::on_qpbTableRefresh_clicked(bool checked /* false */) const
+{
+    RefreshTable();
+} // on_qpbTableRefresh_clicked
+
+const void HtmlExportWidget::on_qpbTextRefresh_clicked(bool checked /* false */) const
+{
+    RefreshText();
+} // on_qpbTextRefresh_clicked
+
+const void HtmlExportWidget::on_qrbStyleTable_clicked(bool checked /* false */) const
+{
+	_qwheHtmlExport.qswStyles->setCurrentIndex(StyleTable);
+} // on_qrbStyleTable_clicked
+
+const void HtmlExportWidget::on_qrbStyleText_clicked(bool checked /* false */) const
+{
+    _qwheHtmlExport.qswStyles->setCurrentIndex(StyleText);
+} // on_qrbStyleText_clicked
+
+const void HtmlExportWidget::on_qsbTableColums_valueChanged(int i)
+{
+    if (i < _qlTableColumns.size()) {
+        RemoveTableColumn();
+    } else {
+        AddTableColumn();
+    } // if else
+} // on_qsbTableColums_valueChanged
+
+HtmlExportWidget::HtmlExportWidget(QWidget *pParent /* NULL */, Qt::WindowFlags pFlags /* 0 */) : QWidget(pParent, pFlags)
+{
+    _qwheHtmlExport.setupUi(this);
+
+    _qwheHtmlExport.qtvCodecs->setModel(&_cmCodecsModel);
+    PreselectCodec("UTF-8");
+
+    InitTableColumns();
+} // HtmlExportWidget
+
+const void HtmlExportWidget::PreselectCodec(const QString &pCodec) const
+{
+    for (int iI = 0; iI < _cmCodecsModel.rowCount(); iI++) {
+        QModelIndex qmiIndex = _cmCodecsModel.index(iI, CodecsModel::ColumnCodec);
+        if (pCodec == _cmCodecsModel.data(qmiIndex)) {
+            _qwheHtmlExport.qtvCodecs->setCurrentIndex(qmiIndex);
+            return;
+        } // if
+    } // for
+} // PreselectCodec
+
+const void HtmlExportWidget::Refresh() const
+{
+    if (_qwheHtmlExport.qrbStyleText->isChecked()) {
+        RefreshText();
+    } else {
+        RefreshTable();
+    } // if else
+} // Refresh
+
+const void HtmlExportWidget::RefreshTable() const
+{
+    // prepare table
+    _qwheHtmlExport.qteTablePreview->clear();
+    QTextCursor qtcCursor = _qwheHtmlExport.qteTablePreview->textCursor();
+    QTextTableFormat qttfTableFormat;
+    QTextTable *qttTablePreview = qtcCursor.insertTable(HEADER_ROW + 1, _qlTableColumns.size(), qttfTableFormat);
+
+    qtcCursor.beginEditBlock();
+
+    // header labels
+    for (int iI = 0; iI < _qlTableColumns.size(); iI++) {
+        InsertTableText(qttTablePreview, HEADER_ROW, iI, _qlTableColumns.at(iI).qleHeader->text());
+    } // for
+
+    // categories
+    ExpInterface::tCategoryIdList tcilCategoryIds;
+    emit VocabularyGetCategoryIds(&tcilCategoryIds);
+
+    // total record count for progress
+    int iTotalRecords = 0;
+    foreach (int iCategoryId, tcilCategoryIds) {
+        int iRecords;
+        emit VocabularyGetRecordCount(iCategoryId, &iRecords);
+        iTotalRecords += iRecords;
+    } // foreach
+    emit ProgressExportSetMax(iTotalRecords);
+    QCoreApplication::processEvents();  // to avoid crash
+
+    QStringList qslMarks;
+    emit VocabularyGetMarks(&qslMarks);
+
+    // preview
+    bool bFirstLine = true;
+    int iRecords = 0;
+    foreach (int iCategoryId, tcilCategoryIds) {
+        if (bFirstLine) {
+            bFirstLine = false;
+        } else {
+            qttTablePreview->appendRows(1);
+            qttTablePreview->mergeCells(qttTablePreview->rows() - 1, 0, 1, _qlTableColumns.size());
+        } // if
+
+        QString qsCategoryName;
+        emit VocabularyGetCategoryName(iCategoryId, &qsCategoryName);
+        qttTablePreview->appendRows(1);
+        int iTableRow = qttTablePreview->rows() - 1;
+        qttTablePreview->mergeCells(iTableRow, 0, 1, _qlTableColumns.size());
+        InsertTableText(qttTablePreview, iTableRow, 0, qsCategoryName);
+
+        // records
+        ExpInterface::tRecordIdList trilRecordIds;
+        emit VocabularyGetRecordIds(iCategoryId, &trilRecordIds);
+        foreach (int iRecordId, trilRecordIds) {
+            qttTablePreview->appendRows(1);
+            iTableRow = qttTablePreview->rows() - 1;
+
+            for (int iColumn = 0; iColumn < _qlTableColumns.size(); iColumn++) {
+                QString qsTemplate = _qlTableColumns.at(iColumn).qleTemplate->text();
+
+                // replace marks for data
+                foreach (QString qsMark, qslMarks) {
+                    QString qsData;
+                    emit VocabularyGetMarkText(iRecordId, qsMark, &qsData);
+                    qsTemplate.replace(qsMark, qsData);
+                } // foreach
+
+                InsertTableText(qttTablePreview, iTableRow, iColumn, qsTemplate);
+            } // for
+
+            iRecords++;
+            emit ProgressExportSetValue(iRecords);
+        } // foreach
+    } // foreach
+
+    qtcCursor.endEditBlock();
+
+    emit ProgressExportSetValue(0);
+} // RefreshTable
+
+const void HtmlExportWidget::RefreshText() const
+{
+    _qwheHtmlExport.qteTextPreview->clear();
+
+    // categories
+    ExpInterface::tCategoryIdList tcilCategoryIds;
+    emit VocabularyGetCategoryIds(&tcilCategoryIds);
+
+    // total record count for progress
+    int iTotalRecords = 0;
+    foreach (int iCategoryId, tcilCategoryIds) {
+        int iRecords;
+        emit VocabularyGetRecordCount(iCategoryId, &iRecords);
+        iTotalRecords += iRecords;
+    } // foreach
+    emit ProgressExportSetMax(iTotalRecords);
+
+    QStringList qslMarks;
+    emit VocabularyGetMarks(&qslMarks);
+
+    // preview
+    bool bFirstLine = true;
+    int iRecords = 0;
+    foreach (int iCategoryId, tcilCategoryIds) {
+        if (bFirstLine) {
+            bFirstLine = false;
+        } else {
+            _qwheHtmlExport.qteTextPreview->append("");
+        } // if
+
+        QString qsCategoryName;
+        emit VocabularyGetCategoryName(iCategoryId, &qsCategoryName);
+        _qwheHtmlExport.qteTextPreview->append(qsCategoryName);
+
+        // records
+        ExpInterface::tRecordIdList trilRecordIds;
+        emit VocabularyGetRecordIds(iCategoryId, &trilRecordIds);
+        foreach (int iRecordId, trilRecordIds) {
+            QString qsTemplate = _qwheHtmlExport.qleTextEdit->text();
+
+            // replace marks for data
+            foreach (QString qsMark, qslMarks) {
+                QString qsData;
+                emit VocabularyGetMarkText(iRecordId, qsMark, &qsData);
+                qsTemplate.replace(qsMark, qsData);
+            } // foreach
+
+            _qwheHtmlExport.qteTextPreview->append(qsTemplate);
+
+            iRecords++;
+            emit ProgressExportSetValue(iRecords);
+        } // foreach
+    } // foreach
+
+    _qwheHtmlExport.qteTextPreview->verticalScrollBar()->setValue(0);
+
+    emit ProgressExportSetValue(0);
+} // RefreshText
+
+const void HtmlExportWidget::RemoveTableColumn()
+{
+    sTableColumn stcColumn = _qlTableColumns.takeLast();
+    stcColumn.qleHeader->deleteLater();
+    stcColumn.qleTemplate->deleteLater();
+} // RemoveTableColumn
