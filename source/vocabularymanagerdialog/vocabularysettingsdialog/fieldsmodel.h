8c3df8cc2883814d01658fdd322d240bd8e30f15
vocabulary-master.cz <vocabulary-master.cz@0d4cc249-e473-e245-99fb-1f26886583cc>
2011-08-24 18:14:13 +0000

    + persistent language editor

--- source/vocabularymanagerdialog/vocabularysettingsdialog/fieldsmodel.h
+++ source/vocabularymanagerdialog/vocabularysettingsdialog/fieldsmodel.h
@@ -23,6 +23,7 @@ class FieldsModel : public QAbstractItemModel
         const void AddRow();
 		virtual QModelIndex index(int row, int column, const QModelIndex &parent = QModelIndex()) const;
 		const void RemoveRow(const int &pRow);
+        virtual int rowCount(const QModelIndex &parent = QModelIndex()) const;
 		const void Swap(const int &pSourceRow, const int &pDestinationRow);
 
     private:
@@ -33,7 +34,6 @@ class FieldsModel : public QAbstractItemModel
         virtual Qt::ItemFlags flags(const QModelIndex &index) const;
         virtual QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
 		virtual QModelIndex parent(const QModelIndex &index) const;
-        virtual int rowCount(const QModelIndex &parent = QModelIndex()) const;
         virtual bool setData(const QModelIndex &index, const QVariant &value, int role /* Qt::EditRole*/);
 }; // FieldsModel
 
