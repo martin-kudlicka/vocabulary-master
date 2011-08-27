8c770eab331de46dc0c47bc93bc32ffc1681ca22
vocabulary-master.cz <vocabulary-master.cz@0d4cc249-e473-e245-99fb-1f26886583cc>
2011-08-27 17:39:44 +0000

    + show license status

--- source/licensedialog.cpp
+++ source/licensedialog.cpp
@@ -4,10 +4,38 @@
 
 const void LicenseDialog::FillLicenseInfo() const
 {
-    _qdlLicenseDialog.qlFirstName->setText(_lLicense->GetFirstName());
-	_qdlLicenseDialog.qlLastName->setText(_lLicense->GetLastName());
-	_qdlLicenseDialog.qlEmail->setText(_lLicense->GetEmail());
-	_qdlLicenseDialog.qlValidTo->setText(_lLicense->GetValidTo().toString(Qt::DefaultLocaleLongDate));
+	if (_lLicense->GetStatus() == License::StatusOk || _lLicense->GetStatus() == License::StatusExpired) {
+		_qdlLicenseDialog.qlFirstName->setText(_lLicense->GetFirstName());
+		_qdlLicenseDialog.qlLastName->setText(_lLicense->GetLastName());
+		_qdlLicenseDialog.qlEmail->setText(_lLicense->GetEmail());
+		_qdlLicenseDialog.qlValidTo->setText(_lLicense->GetValidTo().toString(Qt::DefaultLocaleLongDate));
+
+		_qdlLicenseDialog.qlFirstName->show();
+		_qdlLicenseDialog.qlLastName->show();
+		_qdlLicenseDialog.qlEmail->show();
+		_qdlLicenseDialog.qlValidTo->show();
+	} else {
+		_qdlLicenseDialog.qlFirstName->hide();
+		_qdlLicenseDialog.qlLastName->hide();
+		_qdlLicenseDialog.qlEmail->hide();
+		_qdlLicenseDialog.qlValidTo->hide();
+	} // if else
+
+	switch (_lLicense->GetStatus()) {
+		case License::StatusNone:
+			_qdlLicenseDialog.qlStatus->setText(tr("No license."));
+			break;
+		case License::StatusExpired:
+			_qdlLicenseDialog.qlStatus->setText(tr("License has been expired."));
+			break;
+		case License::StatusInvalid:
+			_qdlLicenseDialog.qlStatus->setText(tr("License is invalid."));
+	} // switch
+	if (_lLicense->GetStatus() == License::StatusOk) {
+		_qdlLicenseDialog.qlStatus->hide();
+	} else {
+		_qdlLicenseDialog.qlStatus->show();
+	} // if else
 } // FillLicenseInfo
 
 LicenseDialog::LicenseDialog(License *pLicense, Settings *pSettings, QWidget *pParent /* NULL */, Qt::WindowFlags pFlags /* 0 */) : QDialog(pParent, pFlags)
