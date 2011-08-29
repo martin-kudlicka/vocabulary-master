eb05731589330457a93782711913bb4bd1e5ade3
vocabulary-master.cz <vocabulary-master.cz@0d4cc249-e473-e245-99fb-1f26886583cc>
2011-08-29 17:23:42 +0000

    - encrypt and sign methods in RSA library

--- common/rsa.h
+++ common/rsa.h
@@ -13,8 +13,6 @@ class
 {
     public:
 		const QByteArray Decrypt(const QByteArray &pPrivateKey, const QByteArray &pContent) const;
-        const QByteArray Encrypt(const QByteArray &pPublicKey, const QByteArray &pContent) const;
-		const QByteArray Sign(const QByteArray &pPrivateKey, const QByteArray &pContent) const;
 		const bool Verify(const QByteArray &pPublicKey, const QByteArray &pContent, const QByteArray &pSignature) const;
 }; // RSA
 
