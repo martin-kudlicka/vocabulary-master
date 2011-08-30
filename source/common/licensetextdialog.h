ab24506afa2abc2b8abffdd7e71d9a29b6b5905c
vocabulary-master.cz <37011117+martin-kudlicka@users.noreply.github.com>
2011-08-30 17:03:52 +0000

    + EULA

--- source/common/licensetextdialog.h
+++ source/common/licensetextdialog.h
@@ -5,16 +5,20 @@
 
 #include <QtGui/QDialog>
 #include "../../common/licensecommon.h"
+#include "settings.h"
 
 class LicenseTextDialog : public QDialog
 {
     public:
-        LicenseTextDialog(const LicenseCommon::tLicenseContentList &pLicenses, QWidget *pParent = NULL, Qt::WindowFlags pFlags = 0);
+        LicenseTextDialog(const LicenseCommon::tLicenseContentList &pLicenses, const Settings *pSettings, QWidget *pParent = NULL, Qt::WindowFlags pFlags = 0);
 
     private:
+        static const int TAB_INVALID = -1;
+
 		Ui::qdLicenseText _qdltLicenseText;
 
         const void ShowLicenses(const LicenseCommon::tLicenseContentList &pLicenses) const;
+        const void ShowMainLicense(const Settings *pSettings) const;
 }; // LicenseTextDialog
 
 #endif // LICENSETEXTDIALOG_H
