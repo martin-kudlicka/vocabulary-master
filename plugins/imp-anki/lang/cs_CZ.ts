3b5655f80caf400c2d4534a2b5877fe6b4277c9b
vocabulary-master.cz <vocabulary-master.cz@0d4cc249-e473-e245-99fb-1f26886583cc>
2011-09-04 09:24:46 +0000

    + imp-anki plug-in Czech translation + imp-plaintext plug-in Czech translation

---
+++ plugins/imp-anki/lang/cs_CZ.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs_CZ" sourcelanguage="en_US">
+<context>
+    <name>DecksModel</name>
+    <message>
+        <location filename="../ankiimportwidget/decksmodel.cpp" line="49"/>
+        <source>Deck</source>
+        <translation>Balíček</translation>
+    </message>
+</context>
+<context>
+    <name>FieldsModel</name>
+    <message>
+        <location filename="../ankiimportwidget/fieldsmodel.cpp" line="59"/>
+        <source>Field</source>
+        <translation>Pole</translation>
+    </message>
+    <message>
+        <location filename="../ankiimportwidget/fieldsmodel.cpp" line="61"/>
+        <source>Mark</source>
+        <translation>Značka</translation>
+    </message>
+</context>
+<context>
+    <name>ModelsModel</name>
+    <message>
+        <location filename="../ankiimportwidget/modelsmodel.cpp" line="53"/>
+        <source>Model</source>
+        <translation>Model</translation>
+    </message>
+</context>
+<context>
+    <name>qwAnkiImport</name>
+    <message>
+        <location filename="../ankiimportwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
