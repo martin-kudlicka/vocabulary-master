b5014824d936106371170da588df8a21f86fc4a0
vocabulary-master.cz <37011117+martin-kudlicka@users.noreply.github.com>
2011-09-04 09:24:22 +0000

    + exp-html plug-in Czech translation

---
+++ plugins/exp-html/lang/cs_CZ.ts
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs_CZ" sourcelanguage="en_US">
+<context>
+    <name>CodecsModel</name>
+    <message>
+        <location filename="../common/codecsmodel.cpp" line="29"/>
+        <source>Codec</source>
+        <translation>Kodek</translation>
+    </message>
+</context>
+<context>
+    <name>ExpHtml</name>
+    <message>
+        <location filename="exp-html.cpp" line="9"/>
+        <source>html (*.html)</source>
+        <translation>html (*.html)</translation>
+    </message>
+    <message>
+        <location filename="exp-html.cpp" line="34"/>
+        <source>html (html)</source>
+        <translation>html (html)</translation>
+    </message>
+</context>
+<context>
+    <name>qwHtmlExport</name>
+    <message>
+        <location filename="htmlexportwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="htmlexportwidget.ui" line="22"/>
+        <source>Style</source>
+        <translation>Styl</translation>
+    </message>
+    <message>
+        <location filename="htmlexportwidget.ui" line="28"/>
+        <source>&amp;Text</source>
+        <translation>T&amp;ext</translation>
+    </message>
+    <message>
+        <location filename="htmlexportwidget.ui" line="38"/>
+        <source>T&amp;able</source>
+        <translation>T&amp;abulka</translation>
+    </message>
+    <message>
+        <location filename="htmlexportwidget.ui" line="79"/>
+        <source>&amp;Template:</source>
+        <translation>Ša&amp;blona:</translation>
+    </message>
+    <message>
+        <location filename="htmlexportwidget.ui" line="94"/>
+        <location filename="htmlexportwidget.ui" line="195"/>
+        <source>Preview</source>
+        <translation>Náhled</translation>
+    </message>
+    <message>
+        <location filename="htmlexportwidget.ui" line="125"/>
+        <location filename="htmlexportwidget.ui" line="226"/>
+        <source>&amp;Refresh</source>
+        <translation>&amp;Obnovit</translation>
+    </message>
+    <message>
+        <location filename="htmlexportwidget.ui" line="152"/>
+        <source> columns</source>
+        <translation>sloupců</translation>
+    </message>
+    <message>
+        <location filename="htmlexportwidget.ui" line="164"/>
+        <source>Header:</source>
+        <translation>Hlavička:</translation>
+    </message>
+    <message>
+        <location filename="htmlexportwidget.ui" line="171"/>
+        <source>Template:</source>
+        <translation>Šablona:</translation>
+    </message>
+</context>
+</TS>
