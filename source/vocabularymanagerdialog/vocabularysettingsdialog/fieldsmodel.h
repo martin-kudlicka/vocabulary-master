2bf14bb235afbc4f0e1fd9d9249740b0108caf2e
vocabulary-master.cz <vocabulary-master.cz@0d4cc249-e473-e245-99fb-1f26886583cc>
2011-09-07 17:07:09 +0000

    + show column in field editor

--- source/vocabularymanagerdialog/vocabularysettingsdialog/fieldsmodel.h
+++ source/vocabularymanagerdialog/vocabularysettingsdialog/fieldsmodel.h
@@ -14,6 +14,7 @@ class FieldsModel : public QAbstractItemModel
             ColumnTemplateName,
             ColumnName,
             ColumnSpeech,
+            ColumnShow,
             ColumnLanguage,
             ColumnCount
         }; // eColumn
