df72165884949b0f49e19392cb1cbf28a19d2b5a
vocabulary-master.cz <vocabulary-master.cz@0d4cc249-e473-e245-99fb-1f26886583cc>
2011-09-12 17:31:57 +0000

    * compilation error fix

--- source/vocabularymanagerdialog/spinboxdelegate.h
+++ source/vocabularymanagerdialog/spinboxdelegate.h
@@ -7,12 +7,11 @@ class SpinBoxDelegate : public QStyledItemDelegate
 {
 	public:
         static const int RECORD_PRIORITY_MAX = 9;
+		static const int RECORD_PRIORITY_MIN = 1;
 
 		SpinBoxDelegate(QObject *pParent = NULL);
 
 	private:
-		static const int RECORD_PRIORITY_MIN = 1;
-
 		virtual QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const;
 		virtual void paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const;
 		virtual void setEditorData(QWidget *editor, const QModelIndex &index) const;
