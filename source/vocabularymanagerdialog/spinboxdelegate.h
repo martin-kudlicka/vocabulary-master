13c1fe41251f15024373f1b651a4d88bbd12b5c4
vocabulary-master.cz <37011117+martin-kudlicka@users.noreply.github.com>
2011-09-12 16:15:43 +0000

    + priority check for new records

--- source/vocabularymanagerdialog/spinboxdelegate.h
+++ source/vocabularymanagerdialog/spinboxdelegate.h
@@ -6,10 +6,11 @@
 class SpinBoxDelegate : public QStyledItemDelegate
 {
 	public:
+        static const int RECORD_PRIORITY_MAX = 9;
+
 		SpinBoxDelegate(QObject *pParent = NULL);
 
 	private:
-		static const int RECORD_PRIORITY_MAX = 9;
 		static const int RECORD_PRIORITY_MIN = 1;
 
 		virtual QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const;
