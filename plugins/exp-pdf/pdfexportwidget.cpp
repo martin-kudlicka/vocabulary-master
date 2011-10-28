7a621b54138c56e434518a5a7aad469e907dc9b0
vocabulary-master.cz <37011117+martin-kudlicka@users.noreply.github.com>
2011-10-28 20:56:03 +0000

    + text codec for encoding

--- plugins/exp-pdf/pdfexportwidget.cpp
+++ plugins/exp-pdf/pdfexportwidget.cpp
@@ -1,49 +1,49 @@
 #include "pdfexportwidget.h"
 
-PdfExportWidget::sEncodingInfo seiEncodings[] = { {"StandardEncoding", PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"MacRomanEncoding", PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"WinAnsiEncoding",  PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"FontSpecific",	   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"ISO8859-2",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"ISO8859-3",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"ISO8859-4",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"ISO8859-5",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"ISO8859-6",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"ISO8859-7",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"ISO8859-8",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"ISO8859-9",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"ISO8859-10",	   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"ISO8859-11",	   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"ISO8859-13",	   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"ISO8859-14",	   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"ISO8859-15",	   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"ISO8859-16",	   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"CP1250",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"CP1251",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"CP1252",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"CP1253",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"CP1254",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"CP1255",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"CP1256",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"CP1257",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"CP1258",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"KOI8-R",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone},
-												  {"GB-EUC-H",		   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetCNS},
-												  {"GB-EUC-V",		   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetCNS},
-												  {"GBK-EUC-H",		   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetCNS},
-												  {"GBK-EUC-V",		   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetCNS},
-												  {"ETen-B5-H",		   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetCNT},
-												  {"ETen-B5-V",		   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetCNT},
-												  {"90ms-RKSJ-H",	   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetJPE},
-												  {"90ms-RKSJ-V",	   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetJPE},
-												  {"90msp-RKSJ-H",	   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetJPE},
-												  {"EUC-H",			   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetJPE},
-												  {"EUC-V",			   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetJPE},
-												  {"KSC-EUC-H",		   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetKRE},
-												  {"KSC-EUC-V",		   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetKRE},
-												  {"KSCms-UHC-H",	   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetKRE},
-												  {"KSCms-UHC-HW-H",   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetKRE},
-												  {"KSCms-UHC-HW-V",   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetKRE} };
+PdfExportWidget::sEncodingInfo seiEncodings[] = { {"StandardEncoding", PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, NULL},
+												  {"MacRomanEncoding", PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "MacRoman"},
+												  {"WinAnsiEncoding",  PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "CP1252"},
+												  {"FontSpecific",	   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, NULL},
+												  {"ISO8859-2",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "ISO-8859-2"},
+												  {"ISO8859-3",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "ISO-8859-3"},
+												  {"ISO8859-4",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "ISO-8859-4"},
+												  {"ISO8859-5",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "ISO-8859-5"},
+												  {"ISO8859-6",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "ISO-8859-6"},
+												  {"ISO8859-7",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "ISO-8859-7"},
+												  {"ISO8859-8",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "ISO-8859-8"},
+												  {"ISO8859-9",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "ISO-8859-9"},
+												  {"ISO8859-10",	   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "ISO-8859-10"},
+												  {"ISO8859-11",	   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "ISO 8859-11"},
+												  {"ISO8859-13",	   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "ISO-8859-13"},
+												  {"ISO8859-14",	   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "ISO-8859-14"},
+												  {"ISO8859-15",	   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "ISO-8859-15"},
+												  {"ISO8859-16",	   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "ISO-8859-16"},
+												  {"CP1250",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "CP1250"},
+												  {"CP1251",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "CP1251"},
+												  {"CP1252",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "CP1252"},
+												  {"CP1253",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "CP1253"},
+												  {"CP1254",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "CP1254"},
+												  {"CP1255",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "CP1255"},
+												  {"CP1256",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "CP1256"},
+												  {"CP1257",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "CP1257"},
+												  {"CP1258",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "CP1258"},
+												  {"KOI8-R",		   PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::EncodingSetNone, "KOI8-R"},
+												  {"GB-EUC-H",		   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetCNS,  "GB2312"},
+												  {"GB-EUC-V",		   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetCNS,  "GB2312"},
+												  {"GBK-EUC-H",		   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetCNS,  "CP936"},
+												  {"GBK-EUC-V",		   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetCNS,  "CP936"},
+												  {"ETen-B5-H",		   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetCNT,  "CP950"},
+												  {"ETen-B5-V",		   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetCNT,  "CP950"},
+												  {"90ms-RKSJ-H",	   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetJPE,  "Shift_JIS"},
+												  {"90ms-RKSJ-V",	   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetJPE,  "Shift_JIS"},
+												  {"90msp-RKSJ-H",	   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetJPE,  "Shift_JIS"},
+												  {"EUC-H",			   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetJPE,  "EUC-JP"},
+												  {"EUC-V",			   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetJPE,  "EUC-JP"},
+												  {"KSC-EUC-H",		   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetKRE,  "EUC-KR"},
+												  {"KSC-EUC-V",		   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetKRE,  "EUC-KR"},
+												  {"KSCms-UHC-H",	   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetKRE,  "cp949"},
+												  {"KSCms-UHC-HW-H",   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetKRE,  "cp949"},
+												  {"KSCms-UHC-HW-V",   PdfExportWidget::EncodingTypeMultibyte,	PdfExportWidget::EncodingSetKRE,  "cp949"} };
 
 PdfExportWidget::sFontInfo sfiFonts[] = { {"Courier",				PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::FontSetNone},
 										  {"Courier-Bold",			PdfExportWidget::EncodingTypeSinglebyte, PdfExportWidget::FontSetNone},
@@ -141,6 +141,17 @@ const PdfExportWidget::eFontSet PdfExportWidget::GetFontSet(const QString &pFont
 	return FontSetNone;
 } // GetFontSet
 
+const char *PdfExportWidget::GetTextCodec(const QString &pEncoding) const
+{
+	for (int iEncoding = 0; iEncoding < sizeof(seiEncodings) / sizeof(sEncodingInfo); iEncoding++) {
+		if (seiEncodings[iEncoding].qsName == pEncoding) {
+			return seiEncodings[iEncoding].cTextCodec;
+		} // if
+	} // for
+
+	return NULL;
+} // GetTextCodec
+
 PdfExportWidget::sFontRoleInfo PdfExportWidget::GetFontInfo(const eFontRole &pRole, const int &pNum /* -1 */) const
 {
 	sFontRoleInfo sfriFontInfo;
@@ -165,6 +176,7 @@ PdfExportWidget::sFontRoleInfo PdfExportWidget::GetFontInfo(const eFontRole &pRo
 	} // switch
 	sfriFontInfo.efsFontSet = GetFontSet(sfriFontInfo.qsFont);
 	sfriFontInfo.eesEncodingSet = GetEncodingSet(sfriFontInfo.qsEncoding);
+	sfriFontInfo.cTextCodec = GetTextCodec(sfriFontInfo.qsEncoding);
 
 	return sfriFontInfo;
 } // GetFontInfo
