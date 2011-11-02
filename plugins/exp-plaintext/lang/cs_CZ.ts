9990ebde261c8d62b85663ea9faa66db32adfc5b
vocabulary-master.cz <vocabulary-master.cz@0d4cc249-e473-e245-99fb-1f26886583cc>
2011-11-02 07:39:00 +0000

    * updated

--- plugins/exp-plaintext/lang/cs_CZ.ts
+++ plugins/exp-plaintext/lang/cs_CZ.ts
@@ -4,7 +4,7 @@
 <context>
     <name>CodecsModel</name>
     <message>
-        <location filename="../common/codecsmodel.cpp" line="29"/>
+        <location filename="../../common/codecsmodel.cpp" line="29"/>
         <source>Codec</source>
         <translation>Kodek</translation>
     </message>
@@ -12,12 +12,12 @@
 <context>
     <name>ExpPlaintext</name>
     <message>
-        <location filename="exp-plaintext.cpp" line="9"/>
+        <location filename="../exp-plaintext.cpp" line="9"/>
         <source>plaintext (*.txt)</source>
         <translation>textový soubor (*.txt)</translation>
     </message>
     <message>
-        <location filename="exp-plaintext.cpp" line="34"/>
+        <location filename="../exp-plaintext.cpp" line="32"/>
         <source>plaintext (txt)</source>
         <translation>textový soubor (txt)</translation>
     </message>
@@ -25,22 +25,26 @@
 <context>
     <name>qwPlaintextExport</name>
     <message>
-        <location filename="plaintextexportwidget.ui" line="14"/>
+        <location filename="../plaintextexportwidget.ui" line="14"/>
         <source>Form</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="plaintextexportwidget.ui" line="49"/>
         <source>&amp;Template:</source>
-        <translation>Š&amp;ablona:</translation>
+        <translation type="obsolete">Š&amp;ablona:</translation>
     </message>
     <message>
-        <location filename="plaintextexportwidget.ui" line="82"/>
+        <location filename="../plaintextexportwidget.ui" line="47"/>
+        <source>Template</source>
+        <translation>Šablona</translation>
+    </message>
+    <message>
+        <location filename="../plaintextexportwidget.ui" line="77"/>
         <source>Preview</source>
         <translation>Náhled</translation>
     </message>
     <message>
-        <location filename="plaintextexportwidget.ui" line="110"/>
+        <location filename="../plaintextexportwidget.ui" line="105"/>
         <source>&amp;Refresh</source>
         <translation>&amp;Obnovit</translation>
     </message>
