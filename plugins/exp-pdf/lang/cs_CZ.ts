fc9d376b392ceae5933a68fa585642e932ec8cb0
vocabulary-master.cz <37011117+martin-kudlicka@users.noreply.github.com>
2011-11-02 07:47:06 +0000

    + Czech language

---
+++ plugins/exp-pdf/lang/cs_CZ.ts
@@ -0,0 +1,114 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs_CZ" sourcelanguage="en_US">
+<context>
+    <name>ExpPdf</name>
+    <message>
+        <location filename="../exp-pdf.cpp" line="21"/>
+        <source>pdf (*.pdf)</source>
+        <translation>pdf (*.pdf)</translation>
+    </message>
+    <message>
+        <location filename="../exp-pdf.cpp" line="153"/>
+        <source>pdf (pdf)</source>
+        <translation>pdf (pdf)</translation>
+    </message>
+</context>
+<context>
+    <name>qwPdfExport</name>
+    <message>
+        <location filename="../pdfexportwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../pdfexportwidget.ui" line="28"/>
+        <source>Style</source>
+        <translation>Styl</translation>
+    </message>
+    <message>
+        <location filename="../pdfexportwidget.ui" line="34"/>
+        <source>&amp;Text</source>
+        <translation>&amp;Text</translation>
+    </message>
+    <message>
+        <location filename="../pdfexportwidget.ui" line="44"/>
+        <source>T&amp;able</source>
+        <translation>T&amp;abulka</translation>
+    </message>
+    <message>
+        <location filename="../pdfexportwidget.ui" line="67"/>
+        <source>Font</source>
+        <translation>Písmo</translation>
+    </message>
+    <message>
+        <location filename="../pdfexportwidget.ui" line="76"/>
+        <source>Category:</source>
+        <translation>Kategorie:</translation>
+    </message>
+    <message>
+        <location filename="../pdfexportwidget.ui" line="91"/>
+        <location filename="../pdfexportwidget.ui" line="121"/>
+        <location filename="../pdfexportwidget.ui" line="155"/>
+        <source>px</source>
+        <translation>px</translation>
+    </message>
+    <message>
+        <location filename="../pdfexportwidget.ui" line="106"/>
+        <location filename="../pdfexportwidget.ui" line="298"/>
+        <source>Template:</source>
+        <translation>Šablona:</translation>
+    </message>
+    <message>
+        <location filename="../pdfexportwidget.ui" line="139"/>
+        <source>General</source>
+        <translation>Obecné</translation>
+    </message>
+    <message>
+        <location filename="../pdfexportwidget.ui" line="148"/>
+        <source>Border:</source>
+        <translation>Okraj:</translation>
+    </message>
+    <message>
+        <location filename="../pdfexportwidget.ui" line="165"/>
+        <source>Page size:</source>
+        <translation>Velikost stránky:</translation>
+    </message>
+    <message>
+        <location filename="../pdfexportwidget.ui" line="175"/>
+        <source>Compression:</source>
+        <translation>Komprese:</translation>
+    </message>
+    <message>
+        <location filename="../pdfexportwidget.ui" line="184"/>
+        <source>Text</source>
+        <translation>Text</translation>
+    </message>
+    <message>
+        <location filename="../pdfexportwidget.ui" line="194"/>
+        <source>Image</source>
+        <translation>Obrázky</translation>
+    </message>
+    <message>
+        <location filename="../pdfexportwidget.ui" line="204"/>
+        <source>Metadata</source>
+        <translation>Metadata</translation>
+    </message>
+    <message>
+        <location filename="../pdfexportwidget.ui" line="231"/>
+        <location filename="../pdfexportwidget.ui" line="250"/>
+        <source>Template</source>
+        <translation>Šablona</translation>
+    </message>
+    <message>
+        <location filename="../pdfexportwidget.ui" line="264"/>
+        <source> columns</source>
+        <translation>sloupce</translation>
+    </message>
+    <message>
+        <location filename="../pdfexportwidget.ui" line="291"/>
+        <source>Header:</source>
+        <translation>Hlavička:</translation>
+    </message>
+</context>
+</TS>
