ef47ded7f4b12cce4a7b22d455ff3fd58b75c44e
kudlicka <37011117+martin-kudlicka@users.noreply.github.com>
2014-05-14 08:52:14 +0000

    * code refactored

--- plugins/exp-pdf/exp-pdf.h
+++ plugins/exp-pdf/exp-pdf.h
@@ -15,7 +15,7 @@ class ExpPdf : public ExpInterface
 
 		struct Font
 		{
-			HPDF_Font         font;
+			HPDF_Font         pdfFont;
 			quint8            size;
 			const QTextCodec *textCodec;
 		}; // Font
@@ -24,17 +24,17 @@ class ExpPdf : public ExpInterface
 
 		PdfExportWidget *_widget;
 
-		        const void    addFont           (const HPDF_Doc &pdf, FontList *fontList, const PdfExportWidget::FontRole &fontRole, const qint8 &num = PdfExportWidget::FONTROLE_NONE) const;
-		virtual const void    beginExport       ()                                                                                                                                       const;
-		        const void    exportTable       (const quint32 &recordId, const HPDF_Page &page, const FontList &fontList, const QStringList &marks)                                     const;
-		        const void    exportText        (const quint32 &recordId, const HPDF_Page &page, const FontList &fontList, const QStringList &marks, const QString &templateText)        const;
-		        const void    initFonts         (const HPDF_Doc &pdf, FontList *fontList, const quint8 &markCount)                                                                       const;
-		        const void    pdfAddPage        (const HPDF_Doc &pdf, HPDF_Page *page, const HPDF_REAL &defaultSize = 0)                                                                 const;
-		        const bool    pdfNextLine       (const HPDF_Doc &pdf, HPDF_Page *page)                                                                                                   const;
-		        const void    pdfSetFont        (const HPDF_Page &page, const HPDF_Font &font, const quint8 &size)                                                                       const;
-		        const void    pdfShowTableHeader(const HPDF_Page &page, const FontList &fontList)                                                                                        const;
-		        const void    pdfShowText       (const HPDF_Page &page, const QString &text, const QTextCodec *textCodec)                                                                const;
-		virtual const QString pluginName        ()                                                                                                                                       const;
+		        const void    addFont           (const HPDF_Doc &pdfDocument, FontList *fontList, const PdfExportWidget::FontRole &fontRole, const qint8 &num = PdfExportWidget::FONTROLE_NONE) const;
+		virtual const void    beginExport       ()                                                                                                                                              const;
+		        const void    exportTable       (const quint32 &recordId, const HPDF_Page &pdfPage, const FontList &fontList, const QStringList &marks)                                         const;
+		        const void    exportText        (const quint32 &recordId, const HPDF_Page &pdfPage, const FontList &fontList, const QStringList &marks, const QString &templateText)            const;
+		        const void    initFonts         (const HPDF_Doc &pdfDocument, FontList *fontList, const quint8 &markCount)                                                                      const;
+		        const void    pdfAddPage        (const HPDF_Doc &pdfDocument, HPDF_Page *pdfPage, const HPDF_REAL &defaultSize = 0)                                                             const;
+		        const bool    pdfNextLine       (const HPDF_Doc &pdfDocument, HPDF_Page *pdfPage)                                                                                               const;
+		        const void    pdfSetFont        (const HPDF_Page &pdfPage, const HPDF_Font &pdfFont, const quint8 &size)                                                                        const;
+		        const void    pdfShowTableHeader(const HPDF_Page &pdfPage, const FontList &fontList)                                                                                            const;
+		        const void    pdfShowText       (const HPDF_Page &pdfPage, const QString &text, const QTextCodec *textCodec)                                                                    const;
+		virtual const QString pluginName        ()                                                                                                                                              const;
 		virtual const void    setupUi           (QWidget *parent);
 
 	private slots:
