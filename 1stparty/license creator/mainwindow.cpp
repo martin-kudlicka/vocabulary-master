fd1eadcd0447cb6e459b4af3a4fdfe1697e8f0cb
kudlicka <37011117+martin-kudlicka@users.noreply.github.com>
2014-05-14 06:59:31 +0000

    * code refactored

--- 1stparty/license creator/mainwindow.cpp
+++ 1stparty/license creator/mainwindow.cpp
@@ -12,7 +12,7 @@ MainWindow::MainWindow(QWidget *parent /* NULL */, Qt::WindowFlags flags /* 0 */
 	_ui.setupUi(this);
 
 	// preset current date + 1 year
-	QDate preset = QDate::currentDate().addYears(1);
+	const QDate preset = QDate::currentDate().addYears(1);
 	_ui.validTo->setDate(preset);
 
 	on_generateUid_clicked();
@@ -43,7 +43,7 @@ const void MainWindow::writeLicensePersonal()
 
 const void MainWindow::on_create_clicked(bool checked /* false */)
 {
-	QString fileName = QFileDialog::getSaveFileName(this, tr("Create license"), QString(), tr("license (*.lic)"));
+	const QString fileName = QFileDialog::getSaveFileName(this, tr("Create license"), QString(), tr("license (*.lic)"));
 	if (fileName.isEmpty())
 	{
 		return;
@@ -61,31 +61,31 @@ const void MainWindow::on_create_clicked(bool checked /* false */)
 	// get encrypt key
 	QFile encryptKeyFile(":/MainWindow/res/mainwindow/encryptpublic.der");
 	encryptKeyFile.open(QIODevice::ReadOnly);
-	QByteArray encryptKey = encryptKeyFile.readAll();
+	const QByteArray encryptKey = encryptKeyFile.readAll();
 
 	// encrypt license
 	CryptoPP::ArraySource encryptKeyBuffer(reinterpret_cast<const byte *>(encryptKey.constData()), encryptKey.size(), true);
-	CryptoPP::RSAES_OAEP_SHA_Encryptor roseEncryptor(encryptKeyBuffer);
+	const CryptoPP::RSAES_OAEP_SHA_Encryptor roseEncryptor(encryptKeyBuffer);
 	CryptoPP::AutoSeededRandomPool randomPool;
 	std::string encryptedString;
-	CryptoPP::ArraySource encryptedBuffer(reinterpret_cast<const byte *>(license.data().constData()), license.size(), true, new CryptoPP::PK_EncryptorFilter(randomPool, roseEncryptor, new CryptoPP::StringSink(encryptedString)));
-	QByteArray encryptedData = QByteArray(encryptedString.c_str(), encryptedString.size());
+	const CryptoPP::ArraySource encryptedBuffer(reinterpret_cast<const byte *>(license.data().constData()), license.size(), true, new CryptoPP::PK_EncryptorFilter(randomPool, roseEncryptor, new CryptoPP::StringSink(encryptedString)));
+	const QByteArray encryptedData = QByteArray(encryptedString.c_str(), encryptedString.size());
 
 	// get sign key
 	QFile signKeyFile(":/MainWindow/res/mainwindow/signprivate.der");
 	signKeyFile.open(QIODevice::ReadOnly);
-	QByteArray signKey = signKeyFile.readAll();
+	const QByteArray signKey = signKeyFile.readAll();
 
 	// sign encrypted license
 	CryptoPP::ArraySource signKeyBuffer(reinterpret_cast<const byte *>(signKey.constData()), signKey.size(), true);
-	CryptoPP::RSASS<CryptoPP::PKCS1v15, CryptoPP::SHA>::Signer signer(signKeyBuffer);
+	const CryptoPP::RSASS<CryptoPP::PKCS1v15, CryptoPP::SHA>::Signer signer(signKeyBuffer);
 	std::string signedString;
-	CryptoPP::ArraySource signedBuffer(reinterpret_cast<const byte *>(encryptedData.constData()), encryptedData.size(), true, new CryptoPP::SignerFilter(randomPool, signer, new CryptoPP::StringSink(signedString)));
-	QByteArray signedData = QByteArray(signedString.c_str(), signedString.size());
+	const CryptoPP::ArraySource signedBuffer(reinterpret_cast<const byte *>(encryptedData.constData()), encryptedData.size(), true, new CryptoPP::SignerFilter(randomPool, signer, new CryptoPP::StringSink(signedString)));
+	const QByteArray signedData = QByteArray(signedString.c_str(), signedString.size());
 
 	// get size of encrypted data
-	qint16 encryptedSize = encryptedData.size();
-	QByteArray encryptedSizeData(reinterpret_cast<const char *>(&encryptedSize), sizeof(encryptedSize));
+	const qint16 encryptedSize = encryptedData.size();
+	const QByteArray encryptedSizeData(reinterpret_cast<const char *>(&encryptedSize), sizeof(encryptedSize));
 
 	// write license to file
 	QFile licenseFile(fileName);
@@ -97,6 +97,6 @@ const void MainWindow::on_create_clicked(bool checked /* false */)
 
 const void MainWindow::on_generateUid_clicked(bool checked /* false */)
 {
-	QUuid uuid = QUuid::createUuid();
+	const QUuid uuid = QUuid::createUuid();
 	_ui.uid->setText(uuid.toString());
 } // on_generateUid_clicked
