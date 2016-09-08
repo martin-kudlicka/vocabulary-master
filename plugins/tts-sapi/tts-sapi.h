166fcc26d3a12e9351b956d04a910b5f317df4a6
kudlicka <37011117+martin-kudlicka@users.noreply.github.com>
2016-09-08 16:56:27 +0000

    * code refactored

--- plugins/tts-sapi/tts-sapi.h
+++ plugins/tts-sapi/tts-sapi.h
@@ -2,24 +2,23 @@
 #define TTSSAPI_H
 
 #include "../common/tts-interface.h"
-#include <QtCore/QHash>
 
 class TTSSAPI : public QObject, private TTSInterface
 {
-    Q_OBJECT
-	Q_PLUGIN_METADATA(IID IID_TTSINTERFACE FILE "tts-sapi.json")
-    Q_INTERFACES(TTSInterface)
+  Q_OBJECT
+  Q_PLUGIN_METADATA(IID IID_TTSINTERFACE FILE "tts-sapi.json")
+  Q_INTERFACES(TTSInterface)
 
-    private:
-		virtual ~TTSSAPI() override;
+  private:
+    virtual ~TTSSAPI() override;
 
-		virtual void                              initialize  ()                                          override;
-        virtual LicenseCommon::LicenseContentList licenseText () const                                    override;
-        virtual TTSPlugin                         pluginId    () const                                    override;
-        virtual QString                           pluginName  () const                                    override;
-		virtual void                              say         (const QString &voice, const QString &text) override;
-		virtual void                              uninitialize()                                          override;
-		virtual VoiceInfoList                     voicesInfo  () const                                    override;
-}; // TTSSAPI
+    virtual void                              initialize  ()                                          override;
+    virtual LicenseCommon::LicenseContentList licenseText () const                                    override;
+    virtual TTSPlugin                         pluginId    () const                                    override;
+    virtual QString                           pluginName  () const                                    override;
+    virtual void                              say         (const QString &voice, const QString &text) override;
+    virtual void                              uninitialize()                                          override;
+    virtual VoiceInfoList                     voicesInfo  () const                                    override;
+};
 
-#endif // TTSSAPI_H
+#endif
