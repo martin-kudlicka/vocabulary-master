194c92f28fbe8bb86479dcdfe3cc8fccceffa7ad
kudlicka <37011117+martin-kudlicka@users.noreply.github.com>
2016-09-08 16:52:40 +0000

    * code refactored

--- plugins/exp-plaintext/plaintextexportwidget.cpp
+++ plugins/exp-plaintext/plaintextexportwidget.cpp
@@ -4,10 +4,10 @@
 
 PlaintextExportWidget::PlaintextExportWidget(QWidget *parent /* nullptr */, Qt::WindowFlags flags /* 0 */) : QWidget(parent, flags)
 {
-	_ui.setupUi(this);
+  _ui.setupUi(this);
 
-	_ui.codecs->setModel(&_codecsModel);
-	preselectCodec("UTF-8");
+  _ui.codecs->setModel(&_codecsModel);
+  preselectCodec("UTF-8");
 }
 
 PlaintextExportWidget::~PlaintextExportWidget()
@@ -16,95 +16,95 @@ PlaintextExportWidget::~PlaintextExportWidget()
 
 QString PlaintextExportWidget::codec() const
 {
-    const QModelIndex modelIndex = _ui.codecs->currentIndex();
-    return _codecsModel.data(modelIndex).toString();
+  const auto modelIndex = _ui.codecs->currentIndex();
+  return _codecsModel.data(modelIndex).toString();
 }
 
 QString PlaintextExportWidget::text() const
 {
-    return _ui.plainPreview->toPlainText();
+  return _ui.plainPreview->toPlainText();
 }
 
 void PlaintextExportWidget::refresh() const
 {
-	_ui.plainPreview->clear();
+  _ui.plainPreview->clear();
 
-	// categories
-	ExpInterface::CategoryIdList categoryIds;
-	emit vocabularyGetCategoryIds(&categoryIds);
+  // categories
+  ExpInterface::CategoryIdList categoryIds;
+  emit vocabularyGetCategoryIds(&categoryIds);
 
-	// total record count for progress
-	quint32 totalRecords = 0;
-	for (const quint8 &categoryId : categoryIds)
-	{
-		quint32 records;
-		emit vocabularyGetRecordCount(categoryId, &records);
-		totalRecords += records;
-	}
-	emit progressExportSetMax(totalRecords);
+  // total record count for progress
+  auto totalRecords = 0;
+  for (auto categoryId : categoryIds)
+  {
+    quint32 records;
+    emit vocabularyGetRecordCount(categoryId, &records);
+    totalRecords += records;
+  }
+  emit progressExportSetMax(totalRecords);
 
-	QStringList marks;
-	emit vocabularyGetMarks(&marks);
+  QStringList marks;
+  emit vocabularyGetMarks(&marks);
 
-	// preview
-	bool firstLine  = true;
-	quint32 records = 0;
-	for (const quint8 &categoryId : categoryIds)
-	{
-		if (firstLine)
-		{
-			firstLine = false;
-		}
-		else
-		{
-			_ui.plainPreview->appendPlainText("");
-		}
+  // preview
+  auto firstLine = true;
+  auto records   = 0;
+  for (auto categoryId : categoryIds)
+  {
+    if (firstLine)
+    {
+      firstLine = false;
+    }
+    else
+    {
+      _ui.plainPreview->appendPlainText("");
+    }
 
-		QString categoryName;
-		emit vocabularyGetCategoryName(categoryId, &categoryName);
-		_ui.plainPreview->appendPlainText(categoryName);
+    QString categoryName;
+    emit vocabularyGetCategoryName(categoryId, &categoryName);
+    _ui.plainPreview->appendPlainText(categoryName);
 
-		// records
-		ExpInterface::RecordIdList recordIds;
-		emit vocabularyGetRecordIds(categoryId, &recordIds);
-		for (const quint32 &recordId : recordIds)
-		{
-			QString templateText = _ui.plainEdit->text();
+    // records
+    ExpInterface::RecordIdList recordIds;
+    emit vocabularyGetRecordIds(categoryId, &recordIds);
+    for (auto recordId : recordIds)
+    {
+      auto templateText = _ui.plainEdit->text();
 
-			// replace marks for data
-			for (const QString &mark : marks)
-			{
-				QString data;
-				emit vocabularyGetMarkText(recordId, mark, &data);
-				templateText.replace(mark, data);
-			}
+      // replace marks for data
+      for (const auto &mark : marks)
+      {
+        QString data;
+        emit vocabularyGetMarkText(recordId, mark, &data);
+        templateText.replace(mark, data);
+      }
 
-			_ui.plainPreview->appendPlainText(templateText);
+      _ui.plainPreview->appendPlainText(templateText);
 
-			records++;
-			emit progressExportSetValue(records);
-		}
-	}
+      records++;
+      emit progressExportSetValue(records);
+    }
+  }
 
-	_ui.plainPreview->verticalScrollBar()->setValue(0);
+  _ui.plainPreview->verticalScrollBar()->setValue(0);
 
-	emit progressExportSetValue(0);
+  emit progressExportSetValue(0);
 }
 
 void PlaintextExportWidget::preselectCodec(const QString &codec) const
 {
-	for (quint8 codecIndex = 0; codecIndex < _codecsModel.rowCount(); codecIndex++)
-	{
-		const QModelIndex modelIndex = _codecsModel.index(codecIndex, static_cast<int>(CodecsModel::Column::Codec));
-		if (codec == _codecsModel.data(modelIndex))
-		{
-			_ui.codecs->setCurrentIndex(modelIndex);
-			return;
-		}
-	}
+  for (auto codecIndex = 0; codecIndex < _codecsModel.rowCount(); codecIndex++)
+  {
+    const auto modelIndex = _codecsModel.index(codecIndex, static_cast<int>(CodecsModel::Column::Codec));
+    if (codec == _codecsModel.data(modelIndex))
+    {
+      _ui.codecs->setCurrentIndex(modelIndex);
+      return;
+    }
+  }
 }
 
 void PlaintextExportWidget::on_refresh_clicked(bool checked /* false */) const
 {
-    refresh();
+  refresh();
 }
--- plugins/exp-plaintext/plaintextexportwidget.h
+++ plugins/exp-plaintext/plaintextexportwidget.h
@@ -8,35 +8,35 @@
 
 class PlaintextExportWidget : public QWidget
 {
-	Q_OBJECT
+  Q_OBJECT
 
-	public:
-		PlaintextExportWidget(QWidget *parent = nullptr, Qt::WindowFlags flags = 0);
+  public:
+    PlaintextExportWidget(QWidget *parent = nullptr, Qt::WindowFlags flags = 0);
 
-        QString codec  () const;
-        QString text   () const;
-        void    refresh() const;
+    QString codec  () const;
+    QString text   () const;
+    void    refresh() const;
 
-	private:
-        CodecsModel               _codecsModel;
-		Ui::PlaintextExportWidget _ui;
+  private:
+    CodecsModel               _codecsModel;
+    Ui::PlaintextExportWidget _ui;
 
-		virtual ~PlaintextExportWidget() override;
+    virtual ~PlaintextExportWidget() override;
 
-        void preselectCodec(const QString &pCodec) const;
+    void preselectCodec(const QString &pCodec) const;
 
-    signals:
-        void progressExportSetMax     (quintptr max)                                               const;
-        void progressExportSetValue   (quintptr value)                                             const;
-        void vocabularyGetCategoryIds (ExpInterface::CategoryIdList *categoryIds)                  const;
-        void vocabularyGetCategoryName(quintptr categoryId, QString *name)                         const;
-        void vocabularyGetMarks       (QStringList *marks)                                         const;
-        void vocabularyGetMarkText    (quintptr recordId, const QString &mark, QString *text)      const;
-        void vocabularyGetRecordCount (quintptr categoryId, quintptr *count)                       const;
-        void vocabularyGetRecordIds   (quintptr categoryId, ExpInterface::RecordIdList *recordIds) const;
+  signals:
+    void progressExportSetMax     (quintptr max)                                               const;
+    void progressExportSetValue   (quintptr value)                                             const;
+    void vocabularyGetCategoryIds (ExpInterface::CategoryIdList *categoryIds)                  const;
+    void vocabularyGetCategoryName(quintptr categoryId, QString *name)                         const;
+    void vocabularyGetMarks       (QStringList *marks)                                         const;
+    void vocabularyGetMarkText    (quintptr recordId, const QString &mark, QString *text)      const;
+    void vocabularyGetRecordCount (quintptr categoryId, quintptr *count)                       const;
+    void vocabularyGetRecordIds   (quintptr categoryId, ExpInterface::RecordIdList *recordIds) const;
 
-    private slots:
-        void on_refresh_clicked(bool checked = false) const;
-}; // PlaintextExportWidget
+  private slots:
+    void on_refresh_clicked(bool checked = false) const;
+};
 
-#endif // PLAINTEXTEXPORTWIDGET_H
+#endif
