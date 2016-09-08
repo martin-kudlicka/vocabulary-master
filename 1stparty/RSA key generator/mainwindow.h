26687e523a77c01cabbd3a594396401d2ffc7573
kudlicka <37011117+martin-kudlicka@users.noreply.github.com>
2016-09-08 16:50:04 +0000

    * tabs to spaces * code refactored

--- 1stparty/RSA key generator/mainwindow.h
+++ 1stparty/RSA key generator/mainwindow.h
@@ -1,26 +1,25 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QtWidgets/QMainWindow>
 #include "ui_mainwindow.h"
 
 class MainWindow : public QMainWindow
 {
-	Q_OBJECT
+  Q_OBJECT
 
-	public:
-		         MainWindow(QWidget *parent = nullptr, Qt::WindowFlags flags = 0);
-		virtual ~MainWindow() override;
+  public:
+             MainWindow(QWidget *parent = nullptr, Qt::WindowFlags flags = 0);
+    virtual ~MainWindow() override;
 
-	private:
-		static const quint16 KEY_SIZE = 3072;
+  private:
+    static const auto KEY_SIZE = 3072;
 
-		Ui::MainWindow _ui;
+    Ui::MainWindow _ui;
 
-	private slots:
-		void on_browsePrivate_clicked(bool checked = false);
-		void on_browsePublic_clicked (bool checked = false);
-		void on_generate_clicked     (bool checked = false) const;
-}; // MainWindow
+  private slots:
+    void on_browsePrivate_clicked(bool checked = false);
+    void on_browsePublic_clicked (bool checked = false);
+    void on_generate_clicked     (bool checked = false) const;
+};
 
-#endif // MAINWINDOW_H
+#endif
