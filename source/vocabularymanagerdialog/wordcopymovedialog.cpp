d4996fcabac2335b4bc8c04bac4a71911cbd152f
Martin <Martin@0d4cc249-e473-e245-99fb-1f26886583cc>
2017-01-11 14:09:17 +0000

    * refactored

--- source/vocabularymanagerdialog/wordcopymovedialog.cpp
+++ source/vocabularymanagerdialog/wordcopymovedialog.cpp
@@ -2,13 +2,13 @@
 
 #include "vocabulary.h"
 
-WordsCopyMoveDialog::WordsCopyMoveDialog(quint8 categoryId, const RowNumList &rowNums, Vocabulary *vocabulary, QWidget *parent /* nullptr */, Qt::WindowFlags flags /* 0 */) : QDialog(parent, flags), _categoriesModel(vocabulary), _oldCategoryId(categoryId), _rowNums(rowNums), _vocabulary(vocabulary)
+WordsCopyMoveDialog::WordsCopyMoveDialog(quintptr categoryId, const RowNumList &rowNums, Vocabulary *vocabulary, QWidget *parent /* Q_NULLPTR */, Qt::WindowFlags flags /* 0 */) : QDialog(parent, flags), _categoriesModel(vocabulary), _oldCategoryId(categoryId), _rowNums(rowNums), _vocabulary(vocabulary)
 {
-	_ui.setupUi(this);
+  _ui.setupUi(this);
 
-	// categories
-	_ui.categories->setModel(&_categoriesModel);
-	connect(_ui.categories->selectionModel(), SIGNAL(selectionChanged(const QItemSelection &, const QItemSelection &)), SLOT(on_categoriesSelectionModel_selectionChanged(const QItemSelection &, const QItemSelection &)));
+  // categories
+  _ui.categories->setModel(&_categoriesModel);
+  connect(_ui.categories->selectionModel(), SIGNAL(selectionChanged(const QItemSelection &, const QItemSelection &)), SLOT(on_categoriesSelectionModel_selectionChanged(const QItemSelection &, const QItemSelection &)));
 }
 
 WordsCopyMoveDialog::~WordsCopyMoveDialog()
@@ -17,30 +17,30 @@ WordsCopyMoveDialog::~WordsCopyMoveDialog()
 
 void WordsCopyMoveDialog::accept()
 {
-    const QModelIndex index    = _ui.categories->currentIndex();
-    const quint8 newCategoryId = _vocabulary->categoryId(index.row());
+  const auto index         = _ui.categories->currentIndex();
+  const auto newCategoryId = _vocabulary->categoryId(index.row());
 
-    if (_ui.copy->isChecked())
-	{
-        for (RowNumList::const_iterator row = _rowNums.constBegin(); row != _rowNums.constEnd(); row++)
-		{
-            const QStringList data = _vocabulary->record(*row);
-            _vocabulary->addRecord(newCategoryId, data);
-        }
+  if (_ui.copy->isChecked())
+  {
+    for (auto row = _rowNums.constBegin(); row != _rowNums.constEnd(); row++)
+    {
+      const auto data = _vocabulary->record(*row);
+      _vocabulary->addRecord(newCategoryId, data);
     }
-	else
-	{
-        for (RowNumList::const_iterator row = _rowNums.constEnd(); row != _rowNums.constBegin();)
-		{
-            row--;
-            _vocabulary->setRecordByRowCategory(_oldCategoryId, *row, newCategoryId);
-        }
+  }
+  else
+  {
+    for (auto row = _rowNums.constEnd(); row != _rowNums.constBegin();)
+    {
+      row--;
+      _vocabulary->setRecordByRowCategory(_oldCategoryId, *row, newCategoryId);
     }
+  }
 
-    QDialog::accept();
+  QDialog::accept();
 }
 
 void WordsCopyMoveDialog::on_categoriesSelectionModel_selectionChanged(const QItemSelection &selected, const QItemSelection &deselected) const
 {
-	_ui.okButton->setEnabled(_ui.categories->currentIndex() != QModelIndex());
+  _ui.okButton->setEnabled(_ui.categories->currentIndex() != QModelIndex());
 }
