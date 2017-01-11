e4c481710648f48814f53c1826ebd4e9a2f8e926
Martin <Martin@0d4cc249-e473-e245-99fb-1f26886583cc>
2017-01-11 12:58:01 +0000

    * refactored

--- source/vocabularyorganizerdialog.h
+++ source/vocabularyorganizerdialog.h
@@ -7,35 +7,35 @@
 
 class VocabularyOrganizerDialog : public QDialog
 {
-    Q_OBJECT
+  Q_OBJECT
 
-    public:
-                 VocabularyOrganizerDialog(VocabularyOrganizer *organizer, QWidget *parent = nullptr, Qt::WindowFlags flags = 0);
-		virtual ~VocabularyOrganizerDialog() override;
+  public:
+             VocabularyOrganizerDialog(VocabularyOrganizer *organizer, QWidget *parent = Q_NULLPTR, Qt::WindowFlags flags = 0);
+    virtual ~VocabularyOrganizerDialog() Q_DECL_OVERRIDE;
 
-	private:
-		Ui::VocabularyOrganizerDialog _ui;
-		VocabularyOrganizer          *_organizer;
-		VocabularyOrganizerModel      _model;
+  private:
+    Ui::VocabularyOrganizerDialog _ui;
+    VocabularyOrganizer          *_organizer;
+    VocabularyOrganizerModel      _model;
 
 #ifndef EDITION_TRY
-		virtual void    accept        () override;
+    virtual void    accept        () Q_DECL_OVERRIDE;
 #endif
 #ifdef EDITION_FREE
-		        void    enableControls() const;
+            void    enableControls() const;
 #endif
 #ifndef EDITION_TRY
-		        QString openPath      () const;
-		virtual void    reject        () override;
+            QString openPath      () const;
+    virtual void    reject        () Q_DECL_OVERRIDE;
 #endif
 
-	private slots:
-		void on_close_clicked                              (bool checked = false);
-		void on_new2_clicked                               (bool checked = false);
+    private Q_SLOTS:
+      void on_close_clicked                              (bool checked = false);
+      void on_new2_clicked                               (bool checked = false);
 #ifndef EDITION_TRY
-		void on_open_clicked                               (bool checked = false);
+      void on_open_clicked                               (bool checked = false);
 #endif
-		void on_vocabulariesSelectionModel_selectionChanged(const QItemSelection &selected, const QItemSelection &deselected);
-}; // VocabularyOrganizerDialog
+      void on_vocabulariesSelectionModel_selectionChanged(const QItemSelection &selected, const QItemSelection &deselected);
+};
 
-#endif // VOCABULARYORGANIZERDIALOG_H
+#endif
