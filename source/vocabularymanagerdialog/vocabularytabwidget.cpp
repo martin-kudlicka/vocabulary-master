d666aa85197d7ac65594f3f86afce2a697f3f3ec
Martin <37011117+martin-kudlicka@users.noreply.github.com>
2017-01-11 13:51:29 +0000

    * refactored

--- source/vocabularymanagerdialog/vocabularytabwidget.cpp
+++ source/vocabularymanagerdialog/vocabularytabwidget.cpp
@@ -5,9 +5,13 @@
 # include <QtWidgets/QSpinBox>
 #endif
 
-VocabularyTabWidget::VocabularyTabWidget(QWidget *pParent /* nullptr */) : QTabWidget(pParent)
+static const auto CATEGORY_PRIORITY_MIN = 1;
+static const auto POSITION_BUTTON_ENABLED = QTabBar::LeftSide;
+static const auto POSITION_BUTTON_PRIORITY = QTabBar::RightSide;
+
+VocabularyTabWidget::VocabularyTabWidget(QWidget *pParent /* Q_NULLPTR */) : QTabWidget(pParent)
 #ifndef EDITION_FREE
-    , _showEnabled(true), _showPriorities(true)
+                                         , _showEnabled(true), _showPriorities(true)
 #endif
 {
 }
@@ -17,64 +21,64 @@ VocabularyTabWidget::~VocabularyTabWidget()
 }
 
 #ifndef EDITION_FREE
-quint8 VocabularyTabWidget::addTab(QWidget *page, const QString &label, bool enabled, quint8 priority)
+quintptr VocabularyTabWidget::addTab(QWidget *page, const QString &label, bool enabled, quint8 priority)
 {
-    const quint8 tab = QTabWidget::addTab(page, label);
+  const auto tab = QTabWidget::addTab(page, label);
 
-	// enabled/disabled
-    if (_showEnabled)
-	{
-        QCheckBox *enabledCheckBox = new QCheckBox(this);
-        enabledCheckBox->setCheckState(enabled ? Qt::Checked : Qt::Unchecked);
-        tabBar()->setTabButton(tab, POSITION_BUTTON_ENABLED, enabledCheckBox);
-        connect(enabledCheckBox, SIGNAL(stateChanged(int)), SLOT(on_enabled_stateChanged(int)));
-    }
+  // enabled/disabled
+  if (_showEnabled)
+  {
+    auto enabledCheckBox = new QCheckBox(this);
+    enabledCheckBox->setCheckState(enabled ? Qt::Checked : Qt::Unchecked);
+    tabBar()->setTabButton(tab, POSITION_BUTTON_ENABLED, enabledCheckBox);
+    connect(enabledCheckBox, SIGNAL(stateChanged(int)), SLOT(on_enabled_stateChanged(int)));
+  }
 
-	// priority
-	if (_showPriorities)
-	{
-		QSpinBox *prioritySpinBox = new QSpinBox(this);
-		prioritySpinBox->setMinimum(CATEGORY_PRIORITY_MIN);
-		prioritySpinBox->setMaximum(CATEGORY_PRIORITY_MAX);
-		prioritySpinBox->setValue(priority);
-		tabBar()->setTabButton(tab, POSITION_BUTTON_PRIORITY, prioritySpinBox);
-		connect(prioritySpinBox, SIGNAL(valueChanged(int)), SLOT(on_priority_valueChanged(int)));
-	}
+  // priority
+  if (_showPriorities)
+  {
+    auto prioritySpinBox = new QSpinBox(this);
+    prioritySpinBox->setMinimum(CATEGORY_PRIORITY_MIN);
+    prioritySpinBox->setMaximum(CATEGORY_PRIORITY_MAX);
+    prioritySpinBox->setValue(priority);
+    tabBar()->setTabButton(tab, POSITION_BUTTON_PRIORITY, prioritySpinBox);
+    connect(prioritySpinBox, SIGNAL(valueChanged(int)), SLOT(on_priority_valueChanged(int)));
+  }
 
-    return tab;
+  return tab;
 }
 
 void VocabularyTabWidget::setShowEnabled(bool enabled)
 {
-    _showEnabled = enabled;
+  _showEnabled = enabled;
 }
 
 void VocabularyTabWidget::setShowPriorities(bool enabled)
 {
-	_showPriorities = enabled;
+  _showPriorities = enabled;
 }
 
 void VocabularyTabWidget::on_enabled_stateChanged(int state) const
 {
-    for (quint8 index = 0; index < count(); index++)
-	{
-        if (sender() == tabBar()->tabButton(index, POSITION_BUTTON_ENABLED))
-		{
-            emit tabEnableChanged(index, static_cast<Qt::CheckState>(state));
-            return;
-        }
+  for (auto index = 0; index < count(); index++)
+  {
+    if (sender() == tabBar()->tabButton(index, POSITION_BUTTON_ENABLED))
+    {
+      emit tabEnableChanged(index, static_cast<Qt::CheckState>(state));
+      return;
     }
+  }
 }
 
 void VocabularyTabWidget::on_priority_valueChanged(int i) const
 {
-	for (int index = 0; index < count(); index++)
-	{
-		if (sender() == tabBar()->tabButton(index, POSITION_BUTTON_PRIORITY))
-		{
-			emit tabPriorityChanged(index, i);
-			return;
-		}
-	}
+  for (auto index = 0; index < count(); index++)
+  {
+    if (sender() == tabBar()->tabButton(index, POSITION_BUTTON_PRIORITY))
+    {
+      emit tabPriorityChanged(index, i);
+      return;
+    }
+  }
 }
 #endif
