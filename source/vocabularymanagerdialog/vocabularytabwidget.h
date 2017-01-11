65bf90aab080790ca8e1aedb9a010876ad24221c
Martin <Martin@0d4cc249-e473-e245-99fb-1f26886583cc>
2017-01-11 13:51:29 +0000

    * refactored

--- source/vocabularymanagerdialog/vocabularytabwidget.h
+++ source/vocabularymanagerdialog/vocabularytabwidget.h
@@ -9,39 +9,35 @@
 class VocabularyTabWidget : public QTabWidget
 {
 #ifndef EDITION_FREE
-    Q_OBJECT
+  Q_OBJECT
 #endif
 
-    public:
+  public:
 #ifndef EDITION_FREE
-        static const quint8 CATEGORY_PRIORITY_MAX = 9;
+    static const auto CATEGORY_PRIORITY_MAX = 9;
 #endif
 
-        VocabularyTabWidget(QWidget *parent = nullptr);
+    VocabularyTabWidget(QWidget *parent = Q_NULLPTR);
 
 #ifndef EDITION_FREE
-        quint8 addTab           (QWidget *page, const QString &label, bool enabled, quint8 priority);
-        void   setShowEnabled   (bool enabled);
-		void   setShowPriorities(bool enabled);
+    quintptr addTab           (QWidget *page, const QString &label, bool enabled, quint8 priority);
+    void     setShowEnabled   (bool enabled);
+    void     setShowPriorities(bool enabled);
 
-    private:
-		static const quint8                  CATEGORY_PRIORITY_MIN    = 1;
-        static const QTabBar::ButtonPosition POSITION_BUTTON_ENABLED  = QTabBar::LeftSide;
-		static const QTabBar::ButtonPosition POSITION_BUTTON_PRIORITY = QTabBar::RightSide;
+  private:
+    bool _showEnabled;
+    bool _showPriorities;
 
-        bool _showEnabled;
-		bool _showPriorities;
+    virtual ~VocabularyTabWidget() Q_DECL_OVERRIDE;
 
-		virtual ~VocabularyTabWidget() override;
+  Q_SIGNALS:
+    void tabEnableChanged  (quintptr index, Qt::CheckState state) const;
+    void tabPriorityChanged(quintptr index, quintptr value)       const;
 
-    signals:
-        void tabEnableChanged  (quint8 index, Qt::CheckState state) const;
-		void tabPriorityChanged(quint8 index, quint8 value)         const;
-
-    private slots:
-        void on_enabled_stateChanged (int state) const;
-		void on_priority_valueChanged(int i)     const;
+    private Q_SLOTS:
+      void on_enabled_stateChanged (int state) const;
+      void on_priority_valueChanged(int i)     const;
 #endif
-}; // VocabularyTabWidget
+};
 
-#endif // VOCABULARYTABWIDGET_H
+#endif
