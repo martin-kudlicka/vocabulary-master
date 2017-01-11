44978f2d7dc6650d05b33579b593c7a300410fad
Martin <37011117+martin-kudlicka@users.noreply.github.com>
2017-01-11 14:09:17 +0000

    * refactored

--- source/vocabularymanagerdialog/wordcopymovedialog.h
+++ source/vocabularymanagerdialog/wordcopymovedialog.h
@@ -7,25 +7,25 @@
 
 class WordsCopyMoveDialog : public QDialog
 {
-	Q_OBJECT
+  Q_OBJECT
 
-	public:
-        using RowNumList = QList<quint32>;
+  public:
+    using RowNumList = QList<quint32>;
 
-		         WordsCopyMoveDialog(quint8 categoryId, const RowNumList &rowNums, Vocabulary *vocabulary, QWidget *parent = nullptr, Qt::WindowFlags flags = 0);
-		virtual ~WordsCopyMoveDialog() Q_DECL_OVERRIDE;
+             WordsCopyMoveDialog(quintptr categoryId, const RowNumList &rowNums, Vocabulary *vocabulary, QWidget *parent = Q_NULLPTR, Qt::WindowFlags flags = 0);
+    virtual ~WordsCopyMoveDialog() Q_DECL_OVERRIDE;
 
-	private:
-		CategoriesModel        _categoriesModel;
-        quint8                 _oldCategoryId;
-		Ui::WordCopyMoveDialog _ui;
-		RowNumList             _rowNums;
-		Vocabulary            *_vocabulary;
+  private:
+    CategoriesModel        _categoriesModel;
+    quintptr               _oldCategoryId;
+    Ui::WordCopyMoveDialog _ui;
+    RowNumList             _rowNums;
+    Vocabulary            *_vocabulary;
 
-		virtual void accept() Q_DECL_OVERRIDE;
+    virtual void accept() Q_DECL_OVERRIDE;
 
-	private Q_SLOTS:
-		void on_categoriesSelectionModel_selectionChanged(const QItemSelection &selected, const QItemSelection &deselected) const;
-}; // WordsCopyMoveDialog
+  private Q_SLOTS:
+    void on_categoriesSelectionModel_selectionChanged(const QItemSelection &selected, const QItemSelection &deselected) const;
+};
 
-#endif // WORDCOPYMOVEDIALOG_H
+#endif
