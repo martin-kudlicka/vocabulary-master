00e095172ac9632b1fb53ff894d5708b9150630b
Martin <37011117+martin-kudlicka@users.noreply.github.com>
2017-01-12 12:47:53 +0000

    * refactored

--- source/vocabularymanagerdialog/wordsimportdialog.h
+++ source/vocabularymanagerdialog/wordsimportdialog.h
@@ -3,50 +3,49 @@
 
 #include <ui_wordsimportdialog.h>
 
-#include "../plugins/common/imp-interface.h"
 #include "common/categoriesmodel.h"
 #include "vocabularymanagerdialog/wordsimportdialog/wordsimportfieldsmodel.h"
 #include "vocabularymanagerdialog/wordsimportdialog/wordsimporteditordelegate.h"
 
+class ImpInterface;
+
 class WordsImportDialog : public QDialog
 {
-	Q_OBJECT
+  Q_OBJECT
 
-	public:
-		         WordsImportDialog(const QString &file, Vocabulary *vocabulary, ImpInterface *plugin, QWidget *parent = nullptr, Qt::WindowFlags flags = 0);
-		virtual ~WordsImportDialog() override;
+  public:
+             WordsImportDialog(const QString &file, Vocabulary *vocabulary, ImpInterface *plugin, QWidget *parent = Q_NULLPTR, Qt::WindowFlags flags = 0);
+    virtual ~WordsImportDialog() Q_DECL_OVERRIDE;
 
-		int exec();
+    int exec() Q_DECL_OVERRIDE;
 
-	private:
-        static const quint8 IMPORT_REFRESHINTERVAL = 20;
+  private:
+    enum class Target
+    {
+      Preview,
+      Vocabulary
+    };
 
-		enum Target
-		{
-			TargetPreview,
-			TargetVocabulary
-		}; // Target
+    bool                      _importing;
+    bool                      _interrupt;
+    CategoriesModel           _categoriesModel;
+    ImpInterface             *_plugin;
+    QString                   _file;
+    WordsImportEditorDelegate _editorDelegate;
+    Ui::WordsImportDialog     _ui;
+    Vocabulary               *_vocabulary;
+    WordsImportFieldsModel    _fieldsModel;
 
-        bool                      _importing;
-        bool                      _interrupt;
-        CategoriesModel           _categoriesModel;
-		ImpInterface             *_plugin;
-		QString                   _file;
-		WordsImportEditorDelegate _editorDelegate;
-		Ui::WordsImportDialog     _ui;
-		Vocabulary               *_vocabulary;
-        WordsImportFieldsModel    _fieldsModel;
+    virtual void accept               () Q_DECL_OVERRIDE;
+            void createFieldEditors   ();
+            void enableControls       () const;
+            void importData           (const Target &target);
+            void preparePreviewColumns() const;
+    virtual void reject               () Q_DECL_OVERRIDE;
 
-        virtual void accept               () override;
-                void createFieldEditors   ();
-                void enableControls       () const;
-		        void importData           (const Target &target);
-		        void preparePreviewColumns() const;
-        virtual void reject               () override;
+  private Q_SLOTS:
+    void on_previewRefresh_clicked                   (bool checked = false);
+    void on_categoriesSelectionModel_selectionChanged(const QItemSelection &selected, const QItemSelection &deselected) const;
+};
 
-	private slots:
-		void on_previewRefresh_clicked                   (bool checked = false);
-        void on_categoriesSelectionModel_selectionChanged(const QItemSelection &selected, const QItemSelection &deselected) const;
-}; // WordsImportDialog
-
-#endif // WORDSIMPORTDIALOG_H
+#endif
