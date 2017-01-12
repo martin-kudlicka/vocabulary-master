ac640e94eec4d6f8a022f108005c407724866156
Martin <37011117+martin-kudlicka@users.noreply.github.com>
2017-01-12 12:24:49 +0000

    * refactored

--- source/vocabularymanagerdialog/wordsexportdialog.h
+++ source/vocabularymanagerdialog/wordsexportdialog.h
@@ -11,33 +11,33 @@
 
 class WordsExportDialog : public QDialog
 {
-	Q_OBJECT
+  Q_OBJECT
 
-	public:
-		         WordsExportDialog(const Vocabulary *vocabulary, const Plugins::ExpPluginList &expPlugins, QWidget *parent = nullptr, Qt::WindowFlags flags = 0);
-		virtual ~WordsExportDialog() override;
+  public:
+             WordsExportDialog(const Vocabulary *vocabulary, const Plugins::ExpPluginList &expPlugins, QWidget *parent = Q_NULLPTR, Qt::WindowFlags flags = 0);
+    virtual ~WordsExportDialog() Q_DECL_OVERRIDE;
 
-	private:
-		CategoriesModel                _categoriesModel;
-		ExpPluginsModel                _expPluginsModel;
-        MarkLineEditPersistentDelegate _markDelegate;
-		Plugins::ExpPluginList         _expPlugins;
-		Ui::WordsExportDialog          _ui;
-		QHash<quint8, quint8>          _expPluginPage;
-		const Vocabulary              *_vocabulary;
-		WordsExportFieldsModel         _fieldsModel;
+  private:
+          CategoriesModel                _categoriesModel;
+          ExpPluginsModel                _expPluginsModel;
+          MarkLineEditPersistentDelegate _markDelegate;
+          Plugins::ExpPluginList         _expPlugins;
+          Ui::WordsExportDialog          _ui;
+          QHash<quint8, quint8>          _expPluginPage;
+    const Vocabulary                    *_vocabulary;
+          WordsExportFieldsModel         _fieldsModel;
 
-	private slots:
-	    const void on_exportButton_clicked                     (bool checked = false);
-		const void on_expPluginsSelectionModel_selectionChanged(const QItemSelection &selected, const QItemSelection &deselected);
-        const void on_plugin_ProgressExportSetMax              (quintptr max)                                              const;
-		const void on_plugin_ProgressExportSetValue            (quintptr value)                                            const;
-        const void on_plugin_VocabularyGetCategoryIds          (ExpInterface::CategoryIdList *categoryIds)                const;
-		const void on_plugin_VocabularyGetCategoryName         (quintptr categoryId, QString *name)                         const;
-        const void on_plugin_VocabularyGetMarks                (QStringList *marks)                                       const;
-		const void on_plugin_VocabularyGetMarkText             (quintptr recordId, const QString &mark, QString *text)     const;
-		const void on_plugin_VocabularyGetRecordCount          (quintptr categoryId, quintptr *count)                        const;
-		const void on_plugin_VocabularyGetRecordIds            (quintptr categoryId, ExpInterface::RecordIdList *recordIds) const;
-}; // WordsExportDialog
+  private Q_SLOTS:
+    const void on_exportButton_clicked                     (bool checked = false);
+    const void on_expPluginsSelectionModel_selectionChanged(const QItemSelection &selected, const QItemSelection &deselected);
+    const void on_plugin_ProgressExportSetMax              (quintptr max)                                               const;
+    const void on_plugin_ProgressExportSetValue            (quintptr value)                                             const;
+    const void on_plugin_VocabularyGetCategoryIds          (ExpInterface::CategoryIdList *categoryIds)                  const;
+    const void on_plugin_VocabularyGetCategoryName         (quintptr categoryId, QString *name)                         const;
+    const void on_plugin_VocabularyGetMarks                (QStringList *marks)                                         const;
+    const void on_plugin_VocabularyGetMarkText             (quintptr recordId, const QString &mark, QString *text)      const;
+    const void on_plugin_VocabularyGetRecordCount          (quintptr categoryId, quintptr *count)                       const;
+    const void on_plugin_VocabularyGetRecordIds            (quintptr categoryId, ExpInterface::RecordIdList *recordIds) const;
+};
 
-#endif // WORDSEXPORTDIALOG_H
+#endif
