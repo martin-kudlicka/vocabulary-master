ca13382d599f6d1fc426607e3d61368b2660f3fc
Martin <Martin@0d4cc249-e473-e245-99fb-1f26886583cc>
2017-01-12 12:50:16 +0000

    * refactored

--- source/vocabularymanagerdialog/wordsimportdialog/wordsimporteditordelegate.h
+++ source/vocabularymanagerdialog/wordsimportdialog/wordsimporteditordelegate.h
@@ -2,18 +2,19 @@
 #define WORDSIMPORTEDITORDELEGATE_H
 
 #include <QtWidgets/QStyledItemDelegate>
-#include "vocabulary.h"
+
+class Vocabulary;
 
 class WordsImportEditorDelegate : public QStyledItemDelegate
 {
-	public:
-		         WordsImportEditorDelegate(const Vocabulary *vocabulary, QObject *parent = nullptr);
-		virtual ~WordsImportEditorDelegate() override;
+  public:
+             WordsImportEditorDelegate(const Vocabulary *vocabulary, QObject *parent = Q_NULLPTR);
+    virtual ~WordsImportEditorDelegate() Q_DECL_OVERRIDE;
 
-	private:
-		const Vocabulary *_vocabulary;
+  private:
+    const Vocabulary *_vocabulary;
 
-		virtual QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
-}; // WordsImportEditorDelegate
+    virtual QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const Q_DECL_OVERRIDE;
+};
 
-#endif // WORDSIMPORTEDITORDELEGATE_H
+#endif
