1bc85f2e8baf6afc736d211437c9384218fd375c
Martin <Martin@0d4cc249-e473-e245-99fb-1f26886583cc>
2017-01-12 12:34:30 +0000

    * refactored

--- source/vocabularymanagerdialog/wordsexportdialog/wordsexportfieldsmodel.cpp
+++ source/vocabularymanagerdialog/wordsexportdialog/wordsexportfieldsmodel.cpp
@@ -1,5 +1,7 @@
 #include "vocabularymanagerdialog/wordsexportdialog/wordsexportfieldsmodel.h"
 
+#include "vocabulary.h"
+
 const QString TEMPLATE_MARK = "${%1}";
 
 WordsExportFieldsModel::WordsExportFieldsModel(const Vocabulary *vocabulary, QObject *parent /* Q_NULLPTR */) : QAbstractItemModel(parent), _vocabulary(vocabulary)
@@ -12,104 +14,102 @@ WordsExportFieldsModel::~WordsExportFieldsModel()
 
 QModelIndex WordsExportFieldsModel::index(int row, int column, const QModelIndex &parent /* QModelIndex() */) const
 {
-    return createIndex(row, column);
+  return createIndex(row, column);
 }
 
 int WordsExportFieldsModel::rowCount(const QModelIndex &parent /* QModelIndex() */) const
 {
-    if (parent == QModelIndex())
-	{
-        // ignore built-in fields
-        quint8 count = 0;
-        for (quint8 vocabularyFieldId : _vocabulary->fieldIds())
-		{
-            if (_vocabulary->fieldBuiltIn(vocabularyFieldId) == VocabularyDatabase::FieldBuiltIn::None)
-			{
-                count++;
-            }
-        }
-        return count;
+  if (parent == QModelIndex())
+  {
+    // ignore built-in fields
+    auto count = 0;
+    for (auto vocabularyFieldId : _vocabulary->fieldIds())
+    {
+      if (_vocabulary->fieldBuiltIn(vocabularyFieldId) == VocabularyDatabase::FieldBuiltIn::None)
+      {
+        count++;
+      }
     }
-	else
-	{
-        return 0;
-    }
+    return count;
+  }
+
+  return 0;
 }
 
 int WordsExportFieldsModel::columnCount(const QModelIndex &parent /* QModelIndex() */) const
 {
-    return ColumnCount;
+  return static_cast<int>(Column::Count);
 }
 
 QVariant WordsExportFieldsModel::data(const QModelIndex &index, int role /* Qt::DisplayRole */) const
 {
-    switch (role)
-	{
-        case Qt::DisplayRole:
-            switch (index.column())
-			{
-				case ColumnLanguage:
-					{
-						const quint8 languageFieldId                     = fieldId(index.row());
-						const VocabularyDatabase::FieldLanguage language = _vocabulary->fieldLanguage(languageFieldId);
-						return _vocabulary->languageName(language);
-					}
-                case ColumnName:
-                    {
-                        const quint8 languageFieldId = fieldId(index.row());
-						return _vocabulary->fieldName(languageFieldId);
-                    }
-                case ColumnMark:
-					const quint8 languageFieldId = fieldId(index.row());
-                    const QString templateText   = _vocabulary->fieldTemplateName(languageFieldId);
-                    return TEMPLATE_MARK.arg(templateText);
-            }
-        default:
-            return QVariant();
-    }
+  switch (role)
+  {
+    case Qt::DisplayRole:
+      switch (index.column())
+      {
+        case static_cast<int>(Column::Language):
+        {
+          const auto languageFieldId = fieldId(index.row());
+          const auto language        = _vocabulary->fieldLanguage(languageFieldId);
+          return _vocabulary->languageName(language);
+        }
+        case static_cast<int>(Column::Name):
+        {
+          const auto languageFieldId = fieldId(index.row());
+          return _vocabulary->fieldName(languageFieldId);
+        }
+        case static_cast<int>(Column::Mark):
+          const auto languageFieldId = fieldId(index.row());
+          const auto templateText    = _vocabulary->fieldTemplateName(languageFieldId);
+          return TEMPLATE_MARK.arg(templateText);
+      }
+    default:
+      return QVariant();
+  }
 }
 
-quint8 WordsExportFieldsModel::fieldId(const int &row) const
+quintptr WordsExportFieldsModel::fieldId(quintptr row) const
 {
-    quint8 num = 0;
-    for (quint8 vocabularyFieldId : _vocabulary->fieldIds())
-	{
-        if (_vocabulary->fieldBuiltIn(vocabularyFieldId) == VocabularyDatabase::FieldBuiltIn::None)
-		{
-            if (row == num)
-			{
-                return vocabularyFieldId;
-            }
-			else
-			{
-                num++;
-            }
-        }
+  auto num = 0;
+  for (auto vocabularyFieldId : _vocabulary->fieldIds())
+  {
+    if (_vocabulary->fieldBuiltIn(vocabularyFieldId) == VocabularyDatabase::FieldBuiltIn::None)
+    {
+      if (row == num)
+      {
+        return vocabularyFieldId;
+      }
+      else
+      {
+        num++;
+      }
     }
+  }
 
-    return VocabularyDatabase::NOT_FOUND;
+  return VocabularyDatabase::NOT_FOUND;
 }
 
 QVariant WordsExportFieldsModel::headerData(int section, Qt::Orientation orientation, int role /* Qt::DisplayRole */) const
 {
-    switch (role)
-	{
-        case Qt::DisplayRole:
-            switch (section)
-			{
-				case ColumnLanguage:
-					return tr("Language");
-                case ColumnName:
-                    return tr("Name");
-				case ColumnMark:
-					return tr("Mark");
-            }
-        default:
-            return QVariant();
-    }
+  switch (role)
+  {
+    case Qt::DisplayRole:
+      switch (section)
+      {
+        case static_cast<int>(Column::Language):
+          return tr("Language");
+        case static_cast<int>(Column::Name):
+          return tr("Name");
+        case static_cast<int>(Column::Mark):
+          return tr("Mark");
+      }
+    default:
+      return QVariant();
+  }
 }
 
 QModelIndex WordsExportFieldsModel::parent(const QModelIndex &index) const
 {
-    return QModelIndex();
+  return QModelIndex();
 }
