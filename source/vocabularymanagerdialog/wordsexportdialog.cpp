1db69640688e38b7a4ad2241059fbd21055fac02
Martin <Martin@0d4cc249-e473-e245-99fb-1f26886583cc>
2017-01-12 12:24:49 +0000

    * refactored

--- source/vocabularymanagerdialog/wordsexportdialog.cpp
+++ source/vocabularymanagerdialog/wordsexportdialog.cpp
@@ -2,30 +2,30 @@
 
 #include "../../common/marklineedit.h"
 
-WordsExportDialog::WordsExportDialog(const Vocabulary *vocabulary, const Plugins::ExpPluginList &expPlugins, QWidget *parent /* nullptr */, Qt::WindowFlags flags /* 0 */) : QDialog(parent, flags), _categoriesModel(vocabulary), _expPluginsModel(&expPlugins), _expPlugins(expPlugins), _vocabulary(vocabulary), _fieldsModel(vocabulary)
+WordsExportDialog::WordsExportDialog(const Vocabulary *vocabulary, const Plugins::ExpPluginList &expPlugins, QWidget *parent /* Q_NULLPTR */, Qt::WindowFlags flags /* 0 */) : QDialog(parent, flags), _categoriesModel(vocabulary), _expPluginsModel(&expPlugins), _expPlugins(expPlugins), _vocabulary(vocabulary), _fieldsModel(vocabulary)
 {
-	_ui.setupUi(this);
+  _ui.setupUi(this);
 
-	// export plugins
-	_ui.expPlugins->setModel(&_expPluginsModel);
-	connect(_ui.expPlugins->selectionModel(), SIGNAL(selectionChanged(const QItemSelection &, const QItemSelection &)), SLOT(on_expPluginsSelectionModel_selectionChanged(const QItemSelection &, const QItemSelection &)));
+  // export plugins
+  _ui.expPlugins->setModel(&_expPluginsModel);
+  connect(_ui.expPlugins->selectionModel(), SIGNAL(selectionChanged(const QItemSelection &, const QItemSelection &)), SLOT(on_expPluginsSelectionModel_selectionChanged(const QItemSelection &, const QItemSelection &)));
 
-	// categories
-	_ui.categories->setModel(&_categoriesModel);
-	// fields
-	_ui.fields->setModel(&_fieldsModel);
-    _ui.fields->setItemDelegateForColumn(WordsExportFieldsModel::ColumnMark, &_markDelegate);
-    for (quint8 row = 0; row < _fieldsModel.rowCount(); row++)
-	{
-        const QModelIndex index = _fieldsModel.index(row, WordsExportFieldsModel::ColumnMark);
-        _ui.fields->openPersistentEditor(index);
-    }
-	for (quint8 column = 0; column < _ui.fields->header()->count(); column++)
-	{
-		_ui.fields->header()->setSectionResizeMode(column, QHeaderView::Stretch);
-	}
+  // categories
+  _ui.categories->setModel(&_categoriesModel);
+  // fields
+  _ui.fields->setModel(&_fieldsModel);
+  _ui.fields->setItemDelegateForColumn(WordsExportFieldsModel::ColumnMark, &_markDelegate);
+  for (auto row = 0; row < _fieldsModel.rowCount(); row++)
+  {
+    const auto index = _fieldsModel.index(row, WordsExportFieldsModel::ColumnMark);
+    _ui.fields->openPersistentEditor(index);
+  }
+  for (auto column = 0; column < _ui.fields->header()->count(); column++)
+  {
+    _ui.fields->header()->setSectionResizeMode(column, QHeaderView::Stretch);
+  }
 
-	_ui.categories->selectAll();
+  _ui.categories->selectAll();
 }
 
 WordsExportDialog::~WordsExportDialog()
@@ -34,100 +34,100 @@ WordsExportDialog::~WordsExportDialog()
 
 const void WordsExportDialog::on_exportButton_clicked(bool checked /* false */)
 {
-    const QModelIndex index    = _ui.expPlugins->currentIndex();
-    const ExpInterface *plugin = _expPlugins.at(index.row()).expInterface;
-    plugin->beginExport();
+  const auto index  = _ui.expPlugins->currentIndex();
+  const auto plugin = _expPlugins.at(index.row()).expInterface;
+  plugin->beginExport();
 }
 
 const void WordsExportDialog::on_expPluginsSelectionModel_selectionChanged(const QItemSelection &selected, const QItemSelection &deselected)
 {
-	const QModelIndex index = _ui.expPlugins->currentIndex();
-	if (!_expPluginPage.contains(index.row()))
-	{
-		// create page for new new plugin
-		QWidget *pluginWidget = new QWidget(_ui.expPluginsStack);
-		QBoxLayout *layout    = new QBoxLayout(QBoxLayout::LeftToRight, pluginWidget);
-		layout->setContentsMargins(QMargins());
+  const auto index = _ui.expPlugins->currentIndex();
+  if (!_expPluginPage.contains(index.row()))
+  {
+    // create page for new new plugin
+    auto pluginWidget = new QWidget(_ui.expPluginsStack);
+    auto layout       = new QBoxLayout(QBoxLayout::LeftToRight, pluginWidget);
+    layout->setContentsMargins(QMargins());
 
-		// insert page to table
-		const quint8 page = _ui.expPluginsStack->addWidget(pluginWidget);
-		_expPluginPage.insert(index.row(), page);
+    // insert page to table
+    const auto page = _ui.expPluginsStack->addWidget(pluginWidget);
+    _expPluginPage.insert(index.row(), page);
 
-		ExpInterface *plugin = _expPlugins.at(index.row()).expInterface;
+    auto plugin = _expPlugins.at(index.row()).expInterface;
 
-        // connections
-    connect(plugin, SIGNAL(ProgressExportSetMax(quintptr)), SLOT(on_plugin_ProgressExportSetMax(quintptr)));
-    connect(plugin, SIGNAL(ProgressExportSetValue(quintptr)), SLOT(on_plugin_ProgressExportSetValue(quintptr)));
-    connect(plugin, SIGNAL(VocabularyGetCategoryIds(ExpInterface::CategoryIdList *)), SLOT(on_plugin_VocabularyGetCategoryIds(ExpInterface::CategoryIdList *)));
-    connect(plugin, SIGNAL(VocabularyGetCategoryName(quintptr, QString *)), SLOT(on_plugin_VocabularyGetCategoryName(quintptr, QString *)));
-    connect(plugin, SIGNAL(VocabularyGetMarks(QStringList *)), SLOT(on_plugin_VocabularyGetMarks(QStringList *)));
-    connect(plugin, SIGNAL(VocabularyGetMarkText(quintptr, const QString &, QString *)), SLOT(on_plugin_VocabularyGetMarkText(quintptr, const QString &, QString *)));
-    connect(plugin, SIGNAL(VocabularyGetRecordCount(quintptr, quintptr *)), SLOT(on_plugin_VocabularyGetRecordCount(quintptr, quintptr *)));
+    // connections
+    connect(plugin, SIGNAL(ProgressExportSetMax(quintptr)),                                 SLOT(on_plugin_ProgressExportSetMax(quintptr)));
+    connect(plugin, SIGNAL(ProgressExportSetValue(quintptr)),                               SLOT(on_plugin_ProgressExportSetValue(quintptr)));
+    connect(plugin, SIGNAL(VocabularyGetCategoryIds(ExpInterface::CategoryIdList *)),       SLOT(on_plugin_VocabularyGetCategoryIds(ExpInterface::CategoryIdList *)));
+    connect(plugin, SIGNAL(VocabularyGetCategoryName(quintptr, QString *)),                 SLOT(on_plugin_VocabularyGetCategoryName(quintptr, QString *)));
+    connect(plugin, SIGNAL(VocabularyGetMarks(QStringList *)),                              SLOT(on_plugin_VocabularyGetMarks(QStringList *)));
+    connect(plugin, SIGNAL(VocabularyGetMarkText(quintptr, const QString &, QString *)),    SLOT(on_plugin_VocabularyGetMarkText(quintptr, const QString &, QString *)));
+    connect(plugin, SIGNAL(VocabularyGetRecordCount(quintptr, quintptr *)),                 SLOT(on_plugin_VocabularyGetRecordCount(quintptr, quintptr *)));
     connect(plugin, SIGNAL(VocabularyGetRecordIds(quintptr, ExpInterface::RecordIdList *)), SLOT(on_plugin_VocabularyGetRecordIds(quintptr, ExpInterface::RecordIdList *)));
 
-		// setup page
-		plugin->setupUi(pluginWidget);
-	}
+    // setup page
+    plugin->setupUi(pluginWidget);
+  }
 
-    // set plugin page
-    _ui.expPluginsStack->setCurrentIndex(_expPluginPage.value(index.row()));
+  // set plugin page
+  _ui.expPluginsStack->setCurrentIndex(_expPluginPage.value(index.row()));
 
-	_ui.exportButton->setEnabled(true);
+  _ui.exportButton->setEnabled(true);
 }
 
 const void WordsExportDialog::on_plugin_ProgressExportSetMax(quintptr max) const
 {
-    _ui.progress->setMaximum(max);
+  _ui.progress->setMaximum(max);
 }
 
 const void WordsExportDialog::on_plugin_ProgressExportSetValue(quintptr value) const
 {
-    _ui.progress->setValue(value);
+  _ui.progress->setValue(value);
 }
 
 const void WordsExportDialog::on_plugin_VocabularyGetCategoryIds(ExpInterface::CategoryIdList *categoryIds) const
 {
-    const QItemSelectionModel *selection = _ui.categories->selectionModel();
-    const QModelIndexList selected       = selection->selectedRows();
+  const auto selection = _ui.categories->selectionModel();
+  const auto selected  = selection->selectedRows();
 
-    for (const QModelIndex &index : selected)
-	{
-        const quint8 categoryId = _vocabulary->categoryId(index.row());
-        categoryIds->append(categoryId);
-    }
+  for (const auto &index : selected)
+  {
+    const auto categoryId = _vocabulary->categoryId(index.row());
+    categoryIds->append(categoryId);
+  }
 }
 
 const void WordsExportDialog::on_plugin_VocabularyGetCategoryName(quintptr categoryId, QString *name) const
 {
-    *name = _vocabulary->categoryName(categoryId);
+  *name = _vocabulary->categoryName(categoryId);
 }
 
 const void WordsExportDialog::on_plugin_VocabularyGetMarks(QStringList *pMarks) const
 {
-    for (quint8 row = 0; row < _fieldsModel.rowCount(); row++)
-	{
-        const QModelIndex editorIndex = _fieldsModel.index(row, WordsExportFieldsModel::ColumnMark);
-        const MarkLineEdit *editor    = qobject_cast<const MarkLineEdit *>(_ui.fields->indexWidget(editorIndex));
-        pMarks->append(editor->text());
-    }
+  for (auto row = 0; row < _fieldsModel.rowCount(); row++)
+  {
+    const auto editorIndex = _fieldsModel.index(row, WordsExportFieldsModel::ColumnMark);
+    const auto editor      = qobject_cast<const MarkLineEdit *>(_ui.fields->indexWidget(editorIndex));
+    pMarks->append(editor->text());
+  }
 }
 
 const void WordsExportDialog::on_plugin_VocabularyGetMarkText(quintptr recordId, const QString &mark, QString *text) const
 {
-    QStringList marks;
-    on_plugin_VocabularyGetMarks(&marks);
-    const quint8 markIndex = marks.indexOf(mark);
-    const quint8 fieldId   = _vocabulary->fieldId(markIndex);
+  QStringList marks;
+  on_plugin_VocabularyGetMarks(&marks);
+  const auto markIndex = marks.indexOf(mark);
+  const auto fieldId   = _vocabulary->fieldId(markIndex);
 
-    *text = _vocabulary->dataText(recordId, fieldId);
+  *text = _vocabulary->dataText(recordId, fieldId);
 }
 
 const void WordsExportDialog::on_plugin_VocabularyGetRecordCount(quintptr categoryId, quintptr *count) const
 {
-    *count = _vocabulary->recordCount(categoryId, _ui.exportEnabledOnly->isChecked());
+  *count = _vocabulary->recordCount(categoryId, _ui.exportEnabledOnly->isChecked());
 }
 
 const void WordsExportDialog::on_plugin_VocabularyGetRecordIds(quintptr categoryId, ExpInterface::RecordIdList *recordIds) const
 {
-    *recordIds = _vocabulary->recordIds(categoryId);
+  *recordIds = _vocabulary->recordIds(categoryId);
 }
