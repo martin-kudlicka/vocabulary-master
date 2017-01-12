313ef0d3d6e2a9023eed1d9562a041c171d3e9cb
Martin <Martin@0d4cc249-e473-e245-99fb-1f26886583cc>
2017-01-12 13:02:21 +0000

    * refactored

--- source/vocabularymanagerdialog/wordsimportdialog/wordsimportfieldsmodel.h
+++ source/vocabularymanagerdialog/wordsimportdialog/wordsimportfieldsmodel.h
@@ -2,38 +2,39 @@
 #define WORDSIMPORTFIELDSMODEL_H
 
 #include <QtCore/QAbstractItemModel>
-#include "vocabulary.h"
 #include <QtCore/QStringList>
+
+class Vocabulary;
 
 class WordsImportFieldsModel : public QAbstractItemModel
 {
-    Q_OBJECT
+  Q_OBJECT
 
-    public:
-        enum Column
-		{
-			ColumnLanguage,
-            ColumnName,
-            ColumnEditor,
-            ColumnCount
-        }; // Column
+  public:
+    enum class Column
+    {
+      Language,
+      Name,
+      Editor,
+      Count
+    };
 
-                 WordsImportFieldsModel(const Vocabulary *vocabulary, QObject *parent = nullptr);
-		virtual ~WordsImportFieldsModel() override;
+             WordsImportFieldsModel(const Vocabulary *vocabulary, QObject *parent = Q_NULLPTR);
+    virtual ~WordsImportFieldsModel() Q_DECL_OVERRIDE;
 
-		virtual QVariant    data    (const QModelIndex &index, int role = Qt::DisplayRole)           const override;
-        virtual QModelIndex index   (int row, int column, const QModelIndex &parent = QModelIndex()) const override;
-        virtual int         rowCount(const QModelIndex &parent = QModelIndex())                      const override;
+    virtual QVariant    data    (const QModelIndex &index, int role = Qt::DisplayRole)           const Q_DECL_OVERRIDE;
+    virtual QModelIndex index   (int row, int column, const QModelIndex &parent = QModelIndex()) const Q_DECL_OVERRIDE;
+    virtual int         rowCount(const QModelIndex &parent = QModelIndex())                      const Q_DECL_OVERRIDE;
 
-    private:
-		      QStringList _editorData;
-        const Vocabulary *_vocabulary;
+  private:
+          QStringList _editorData;
+    const Vocabulary *_vocabulary;
 
-        virtual int           columnCount(const QModelIndex &parent = QModelIndex())                            const override;
-		virtual Qt::ItemFlags flags      (const QModelIndex &index)                                             const override;
-        virtual QVariant      headerData (int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
-        virtual QModelIndex   parent     (const QModelIndex &index)                                             const override;
-		virtual bool          setData    (const QModelIndex &index, const QVariant &value, int role = Qt::EditRole)   override;
-}; // WordsImportFieldsModel
+    virtual int           columnCount(const QModelIndex &parent = QModelIndex())                            const Q_DECL_OVERRIDE;
+    virtual Qt::ItemFlags flags      (const QModelIndex &index)                                             const Q_DECL_OVERRIDE;
+    virtual QVariant      headerData (int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const Q_DECL_OVERRIDE;
+    virtual QModelIndex   parent     (const QModelIndex &index)                                             const Q_DECL_OVERRIDE;
+    virtual bool          setData    (const QModelIndex &index, const QVariant &value, int role = Qt::EditRole)   Q_DECL_OVERRIDE;
+};
 
-#endif // WORDSIMPORTFIELDSMODEL_H
+#endif
