ebb1903c9fae3ae7f17224e98ac1da703811f782
Martin <Martin@0d4cc249-e473-e245-99fb-1f26886583cc>
2017-01-28 22:51:09 +0000

    * version info fixed + MApplication to set application info

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,9 @@
+#include <MkWidgets/MApplication>
 #include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
-  QApplication application(argc, argv);
-  application.setOrganizationName("MartinK");
-  application.setApplicationName("Vocabulary Master");
+  MApplication application(argc, argv);
 
   const MainWindow mainWindow;
 
